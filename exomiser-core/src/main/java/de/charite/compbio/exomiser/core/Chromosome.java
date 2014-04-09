fa345852327e04abf8d091daec73807e4387231b
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2014-04-09 16:41:35 +0100

    Removed IntelliJ project files.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/Chromosome.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/Chromosome.java
@@ -7,10 +7,10 @@
 package de.charite.compbio.exomiser.core;
 
 /**
- * Enum representation of a Chromosome. 
+ * Enum representation of a Chromosome.  
  * @author Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
  */
 public enum Chromosome {
-//    TODO: This might be a bad way to represent these. 
+//    TODO: Could there be a better way to represent these?
     CHR_1, CHR_2, CHR_3, CHR_4, CHR_5, CHR_6, CHR_7, CHR_9, CHR_10,CHR_11, CHR_12, CHR_13, CHR_14,CHR_15,CHR_16,CHR_17,CHR_18,CHR_19,CHR_20, CHR_21, CHR_22, CHR_X, CHR_Y, CHR_M;
 }
