3e21366df010a99ad9439bb05b7ad231623d745b
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2014-04-22 14:23:24 +0100

    Added MetaDataParser, SQL migrations and wired it into the ResourceParserHandler

---
+++ exomiser-db/src/main/java/db/migration/postgres/V2_9__Insert_metadata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package db.migration.postgres;
+
+import com.googlecode.flyway.core.api.migration.jdbc.JdbcMigration;
+import java.io.FileReader;
+import java.sql.Connection;
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
+
+/**
+ * Flyway java migration for importing metadata data into the exomiser PostgreSQL instance.
+ * 
+ * @author Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
+ */
+public class V2_9__Insert_metadata implements JdbcMigration {
+    
+    @Override
+    public void migrate(Connection connection) throws Exception {
+        CopyManager copyManager = new CopyManager((BaseConnection) connection);
+        try (FileReader fileReader = new FileReader("data/metadata.pg")) {
+            copyManager.copyIn("COPY string from STDIN WITH DELIMITER '|';", fileReader, 1024);
+        }
+    }
+}
