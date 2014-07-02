341e216761c1d57e969a12bf7c1df9b741c1f672
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2014-07-02 11:42:54 +0100

    Fixed mangled comment in PriorityType.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/priority/PriorityType.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/priority/PriorityType.java
@@ -8,8 +8,6 @@ public enum PriorityType {
    
     /**
      * Flag to represent results of filtering against an inheritance pattern.
-     *//**
-     * Flag to represent results of filtering against an inheritance pattern.
      */
     INHERITANCE_MODE_PRIORITY("inheritance-mode", ScoringMode.RAW_SCORE),
     /**
