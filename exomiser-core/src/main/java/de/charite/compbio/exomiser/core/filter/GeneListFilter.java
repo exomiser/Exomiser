ba52b4808b3c7ab8ae3b4679f3f99769ac0e3c09
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2014-10-22 12:58:51 +0100

    Changed List to Set in GeneListFilter as this avoids duplicates and provides better performance on contains if the user specifies a large number of genes.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/filter/GeneListFilter.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/filter/GeneListFilter.java
@@ -1,24 +1,20 @@
 package de.charite.compbio.exomiser.core.filter;
 
-import de.charite.compbio.exomiser.core.model.GeneticInterval;
 import de.charite.compbio.exomiser.core.model.VariantEvaluation;
-import jannovar.exome.Variant;
-import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * VariantFilter variants according to a linkage interval. For instance, if the
- * interval is chr2:12345-67890, then we would only keep variants located
- * between positions 12345 and 67890 on chromosome 2. All other variants are
- * discarded.
- * <P>
- * The interval must be given as chr2:12345-67890 (format), otherwise, an error
- * message is given and no filtering is done.
+ * VariantFilter to remove any variants belonging to genes not on a user-entered
+ * list of genes.
  *
- * @author Peter N Robinson
- * @version 0.08 (April 28, 2013)
+ * Note: this could be done as a GeneFilter but will be most efficient to run as
+ * the first variantFilter
+ *
+ * @author Damian Smedley
+ * @author Jules Jacobsen
  */
 public class GeneListFilter implements VariantFilter {
 
@@ -30,23 +26,12 @@ public class GeneListFilter implements VariantFilter {
     private final FilterResult passFilterResult = new GeneListFilterResult(1f, FilterResultStatus.PASS);
     private final FilterResult failedFilterResult = new GeneListFilterResult(0f, FilterResultStatus.FAIL);
 
-    private List<Integer> genesToKeep;
+    private final Set<Integer> genesToKeep;
 
-    /**
-     * Constructor defining the genetic interval.
-     *
-     * @param interval the interval based on a String such as chr2:12345-67890.
-     */
-    public GeneListFilter(List<Integer> genesToKeep) {
+    public GeneListFilter(Set<Integer> genesToKeep) {
         this.genesToKeep = genesToKeep;
-
     }
 
-    /**
-     * @return an integer constant (as defined in exomizer.common.Constants)
-     * that will act as a flag to generate the output HTML dynamically depending
-     * on the filters that the user has chosen.
-     */
     @Override
     public FilterType getFilterType() {
         return filterType;
@@ -54,7 +39,7 @@ public class GeneListFilter implements VariantFilter {
 
     @Override
     public FilterResult runFilter(VariantEvaluation variantEvaluation) {
-        if (genesToKeep.contains(variantEvaluation.getVariant().getEntrezGeneID())){
+        if (genesToKeep.contains(variantEvaluation.getVariant().getEntrezGeneID())) {
             return passFilterResult;
         }
         return failedFilterResult;
