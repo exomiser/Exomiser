5ef90e3c6ba9f035ec21b20b41cb879d7ae74ffe
max <max@vsize.de>
2014-12-22 18:41:14 +0100

    Language fix. Otherwise test will fail on german computers because , is used instead of .

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/filter/FilterReportFactory.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/filter/FilterReportFactory.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Locale;
 
 /**
  * Factory class for producing {@code FilterReport} lists from the list of
@@ -22,6 +23,7 @@ import org.slf4j.LoggerFactory;
  * @author Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
  */
 public class FilterReportFactory {
+	
 
     private static final Logger logger = LoggerFactory.getLogger(FilterReportFactory.class);
 
