69eea75f0dfdec994c2fb3c210e6acadc4688c3e
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2015-02-10 17:11:58 +0000

    Fix for borked exomiser-web build. Changed phenomiserDataDir to phenixDataDir in ExomiserConfig and TestExomiserConfig

--- exomiser-web/src/main/java/de/charite/compbio/exomiser/web/config/ExomiserConfig.java
+++ exomiser-web/src/main/java/de/charite/compbio/exomiser/web/config/ExomiserConfig.java
@@ -71,22 +71,22 @@ public class ExomiserConfig {
     }
 
     @Bean
-    public Path phenomizerDataDirectory() {
-        Path phenomizerDataDirectory = dataPath().resolve(env.getProperty("phenomizerDataDir"));
-        logger.info("phenomizerDataDirectory: {}", phenomizerDataDirectory.toAbsolutePath());
-        return phenomizerDataDirectory;
+    public Path phenixDataDirectory() {
+        Path phenixDataDirectory = dataPath().resolve(env.getProperty("phenomizerDataDir"));
+        logger.info("phenixDataDirectory: {}", phenixDataDirectory.toAbsolutePath());
+        return phenixDataDirectory;
     }
 
     @Bean
     public Path hpoOntologyFilePath() {
-        Path hpoOntologyFilePath = phenomizerDataDirectory().resolve(env.getProperty("hpoOntologyFile"));
+        Path hpoOntologyFilePath = phenixDataDirectory().resolve(env.getProperty("hpoOntologyFile"));
         logger.info("hpoOntologyFilePath: {}", hpoOntologyFilePath.toAbsolutePath());
         return hpoOntologyFilePath;
     }
 
     @Bean
     public Path hpoAnnotationFilePath() {
-        Path hpoAnnotationFilePath = phenomizerDataDirectory().resolve(env.getProperty("hpoAnnotationFile"));
+        Path hpoAnnotationFilePath = phenixDataDirectory().resolve(env.getProperty("hpoAnnotationFile"));
         logger.info("hpoAnnotationFilePath: {}", hpoAnnotationFilePath.toAbsolutePath());
         return hpoAnnotationFilePath;
     }
