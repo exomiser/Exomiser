eacff80d88e609ea023c71487db81d916a5bfc9c
Damian Smedley <ds5@sanger.ac.uk>
2015-03-20 13:35:44 +0000

    fixes to load exac frequencies into database and to remove all use of Constants for unitilialised floats etc

--- exomiser-db/src/main/resources/db/migration/h2/V2.0__Insert_frequency.sql
+++ exomiser-db/src/main/resources/db/migration/h2/V2.0__Insert_frequency.sql
@@ -1,2 +1,2 @@
-INSERT INTO frequency SELECT * FROM CSVREAD('${import.path}/frequency.pg', 'chromosome|position|ref|alt|rsid|dbsnpmaf|espmeamaf|espaamaf|espallmaf','charset=UTF-8 fieldDelimiter='' fieldSeparator=| nullString=NULL');
+INSERT INTO frequency SELECT * FROM CSVREAD('${import.path}/frequency.pg', 'chromosome|position|ref|alt|rsid|dbsnpmaf|espmeamaf|espaamaf|espallmaf|exacafrmaf|exacamrmaf|exaceasmaf|exacfinmaf|exacnfemaf|exacothmaf|exacsasmaf','charset=UTF-8 fieldDelimiter='' fieldSeparator=| nullString=NULL');
 
