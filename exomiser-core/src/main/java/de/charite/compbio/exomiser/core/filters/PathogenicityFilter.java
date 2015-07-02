fefecdcf9d1fdd7b4a5635a73116b3d23dac82a1
Damian Smedley <ds5@sanger.ac.uk>
2015-07-02 12:23:02 +0100

    Put back to original state on dev

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/filters/PathogenicityFilter.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/filters/PathogenicityFilter.java
@@ -18,19 +18,14 @@ import org.slf4j.LoggerFactory;
  * VariantFilter variants according to their predicted pathogenicity. There are
  * two components to this, which may better be separated in later versions of
  * this software, but I think there are more advantages to keeping them all in
- * one class.
- * <P>
- * There are variants such as splice site variants, which we can assume are in
- * general pathogenic. We at the moment do not need to use any particular
- * software to evaluate this, we merely take the variant class from the Jannovar
- * code.
- * <P>
- * For missense mutations, we will use the predictions of MutationTaster,
- * polyphen, and SIFT taken from the data from the dbNSFP project.
- * <P>
- * The code therefore removes mutations judged not to be pathogenic (intronic,
- * etc.), and assigns each other mutation an overall pathogenicity score defined
- * on the basis of "medical genetic intuition".
+ * one class. <P> There are variants such as splice site variants, which we can
+ * assume are in general pathogenic. We at the moment do not need to use any
+ * particular software to evaluate this, we merely take the variant class from
+ * the Jannovar code. <P> For missense mutations, we will use the predictions of
+ * MutationTaster, polyphen, and SIFT taken from the data from the dbNSFP
+ * project. <P> The code therefore removes mutations judged not to be pathogenic
+ * (intronic, etc.), and assigns each other mutation an overall pathogenicity
+ * score defined on the basis of "medical genetic intuition".
  *
  * @author Peter N Robinson
  * @author Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
@@ -40,9 +35,7 @@ public class PathogenicityFilter implements VariantFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(PathogenicityFilter.class);
     private static final FilterType filterType = FilterType.PATHOGENICITY_FILTER;
-
     public static final float DEFAULT_PATHOGENICITY_THRESHOLD = 0.5f;
-    
     private final boolean removePathFilterCutOff;
 
     /**
@@ -70,19 +63,17 @@ public class PathogenicityFilter implements VariantFilter {
      * VariantFilter variants based on their calculated pathogenicity. Those
      * that pass have a pathogenicity score assigned to them. The failed ones
      * are deemed to be non-pathogenic and marked as such.
-     *
-     */
+     *     
+*/
     @Override
     public FilterResult runFilter(VariantEvaluation variantEvaluation) {
         PathogenicityData pathData = variantEvaluation.getPathogenicityData();
         VariantEffect variantEffect = variantEvaluation.getVariantEffect();
-
         float filterScore = calculateFilterScore(variantEffect, pathData);
-
         if (removePathFilterCutOff) {
             return returnPassResult(filterScore);
         }
-        if (variantIsPredictedPathogenic(variantEffect,pathData)) {
+        if (variantIsPredictedPathogenic(variantEffect)) {
             return returnPassResult(filterScore);
         }
         return returnFailResult(filterScore);
@@ -90,14 +81,14 @@ public class PathogenicityFilter implements VariantFilter {
 
     /**
      * Creates the PathogenicityScore data
-     *
-     * @param variantEffect
+     *     
+* @param variantEffect
      * @param pathogenicityData
      * @return
      */
     protected float calculateFilterScore(VariantEffect variantEffect, PathogenicityData pathogenicityData) {
-        if (pathogenicityData.hasPredictedScore(PathogenicitySource.CADD)) {
-            return pathogenicityData.getCaddScore().getScore();
+        if (variantEffect == VariantEffect.MISSENSE_VARIANT) {
+            return returnMissenseScore(pathogenicityData);
         } else {
             //return the default score - in time we might want to use the predicted score if there are any and handle things like the missense variants.
             return VariantTypePathogenicityScores.getPathogenicityScoreOf(variantEffect);
@@ -113,7 +104,7 @@ public class PathogenicityFilter implements VariantFilter {
 
     private float returnMostPathogenicPredictedScore(PathogenicityData pathogenicityData) {
         PathogenicityScore mostPathogenicPredictedScore = pathogenicityData.getMostPathogenicScore();
-        //Thanks to SIFT being about tolerance rather than pathogenicity, the score is inverted
+//Thanks to SIFT being about tolerance rather than pathogenicity, the score is inverted
         if (mostPathogenicPredictedScore.getClass() == SiftScore.class) {
             return 1 - mostPathogenicPredictedScore.getScore();
         }
@@ -123,13 +114,16 @@ public class PathogenicityFilter implements VariantFilter {
     /**
      * @param variantEffect
      * @param pathData
-     * @return true if the variant being analysed passes the runFilter (e.g., has high quality )
+     * @return true if the variant being analysed passes the runFilter (e.g.,
+     * has high quality )
      */
-    protected boolean variantIsPredictedPathogenic(VariantEffect variantEffect, PathogenicityData pathogenicityData) {
-        if (pathogenicityData.hasPredictedScore(PathogenicitySource.CADD)) {
+    protected boolean variantIsPredictedPathogenic(VariantEffect variantEffect) {
+        if (variantEffect == VariantEffect.MISSENSE_VARIANT) {
+            //we're making the assumption that a miissense variant is always
+            //potentially pathogenic as the prediction scores are predictions,
+            //we'll leave it up to the user to decide
             return true;
-        }
-        else {
+        } else {
             return VariantTypePathogenicityScores.getPathogenicityScoreOf(variantEffect) >= DEFAULT_PATHOGENICITY_THRESHOLD;
         }
     }
@@ -170,5 +164,4 @@ public class PathogenicityFilter implements VariantFilter {
     public String toString() {
         return String.format("%s filter: removePathFilterCutOff=%s", filterType, removePathFilterCutOff);
     }
-
 }
