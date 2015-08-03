abda9b82fd86c1e054119be2b2fcb69af63d7719
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2015-08-03 14:31:18 +0100

    Changed name of AbstractPathogenicityScore to BasePathogenicityScore as it wasn't abstract.

---
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/pathogenicity/BasePathogenicityScore.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.charite.compbio.exomiser.core.model.pathogenicity;
+
+/**
+ *
+ * @author Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
+ */
+public class BasePathogenicityScore implements PathogenicityScore {
+
+    protected final float score;
+    protected final PathogenicitySource source;
+
+    public BasePathogenicityScore(float score, PathogenicitySource source) {
+        this.score = score;
+        this.source = source;
+    }
+
+    @Override
+    public float getScore() {
+        return score;
+    }
+
+    @Override
+    public PathogenicitySource getSource() {
+        return source;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Float.floatToIntBits(this.score);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BasePathogenicityScore other = (BasePathogenicityScore) obj;
+        if (Float.floatToIntBits(this.score) != Float.floatToIntBits(other.score)) {
+            return false;
+        }
+        return true;
+    }
+
+    //Higher scores are more likely pathogenic so this is the reverse of what's normal (we're using a probablility of pathogenicity)
+    //comparable requires a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object
+    protected static final int MORE_PATHOGENIC = -1;
+    protected static final int EQUALS = 0;
+    protected static final int LESS_PATHOGENIC = 1;
+
+    /**
+     * For the purposes of this comparator scores are ranked on a scale of 0 to
+     * 1 where 0 is considered probably non-pathogenic and 1 probably
+     * pathogenic. The comparator will rank the more pathogenic
+     * PathogenicityScore higher than a less pathogenic score such that in a
+     * sorted list the most pathogenic score will come first.
+     *
+     * Note: this class has a natural ordering that is inconsistent with equals.
+     *
+     * @param other
+     * @return
+     */
+    @Override
+    public int compareTo(PathogenicityScore other) {
+        float otherScore = other.getScore();
+        float thisScore = score;
+        //remember if we're comparing this against a SIFT score we need to invert the
+        //SIFT score otherwise the comparison will be inverted
+        if (other.getClass() == SiftScore.class) {
+            otherScore = 1f - other.getScore();
+        }
+        if (this.getClass() == SiftScore.class) {
+            thisScore = 1f - score;
+        }
+
+        if (thisScore == otherScore) {
+            return EQUALS;
+        }
+        if (thisScore > otherScore) {
+            return MORE_PATHOGENIC;
+        }
+        return LESS_PATHOGENIC;
+    }
+}
