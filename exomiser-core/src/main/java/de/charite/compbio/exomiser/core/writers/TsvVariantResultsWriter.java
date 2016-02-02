9ba036db2b737f8eb0741c909193e2995292ac95
Max Schubach <max.schubach@charite.de>
2016-02-02 09:49:17 +0100

    Renaming MNCDS into ReMM_SCORE in the TSVVariant Output

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/writers/TsvVariantResultsWriter.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/writers/TsvVariantResultsWriter.java
@@ -74,7 +74,7 @@ public class TsvVariantResultsWriter implements ResultsWriter {
             .withRecordSeparator("\n")
             .withIgnoreSurroundingSpaces(true)
             .withHeader("#CHROM", "POS", "REF", "ALT", "QUAL", "FILTER", "GENOTYPE", "COVERAGE", "FUNCTIONAL_CLASS", "HGVS", "EXOMISER_GENE",
-                    "CADD(>0.483)", "POLYPHEN(>0.956|>0.446)", "MUTATIONTASTER(>0.94)", "SIFT(<0.06)", "MNCDS",
+                    "CADD(>0.483)", "POLYPHEN(>0.956|>0.446)", "MUTATIONTASTER(>0.94)", "SIFT(<0.06)", "ReMM_SCORE",
                     "DBSNP_ID", "MAX_FREQUENCY", "DBSNP_FREQUENCY", "EVS_EA_FREQUENCY", "EVS_AA_FREQUENCY",
                     "EXAC_AFR_FREQ", "EXAC_AMR_FREQ", "EXAC_EAS_FREQ", "EXAC_FIN_FREQ", "EXAC_NFE_FREQ", "EXAC_SAS_FREQ", "EXAC_OTH_FREQ",
                     "EXOMISER_VARIANT_SCORE", "EXOMISER_GENE_PHENO_SCORE", "EXOMISER_GENE_VARIANT_SCORE", "EXOMISER_GENE_COMBINED_SCORE");
