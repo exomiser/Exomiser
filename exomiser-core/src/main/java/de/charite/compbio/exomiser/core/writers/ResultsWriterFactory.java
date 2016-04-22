628679e224f877e026f5250e63dab5b648949abe
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-04-22 14:57:44 +0100

    Changed bean name of TemplateEngine in ResultsWriterFactory to coreTemplateEngine so as not to clash with that used in the web module. Springified ResultsWriterFactoryTest and added Spring config file.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/writers/ResultsWriterFactory.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/writers/ResultsWriterFactory.java
@@ -1,4 +1,23 @@
 /*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,7 +39,7 @@ import org.thymeleaf.TemplateEngine;
 public class ResultsWriterFactory {
 
     @Autowired
-    private TemplateEngine templateEngine;
+    private TemplateEngine coreTemplateEngine;
 
     /**
      * Build {@link ResultsWriter} for the given {@link OutputFormat}.
@@ -32,7 +51,7 @@ public class ResultsWriterFactory {
     public ResultsWriter getResultsWriter(OutputFormat outputFormat) {
         switch (outputFormat){
             case HTML:
-                return new HtmlResultsWriter(templateEngine);
+                return new HtmlResultsWriter(coreTemplateEngine);
             case TSV_GENE:
                 return new TsvGeneResultsWriter();
             case TSV_VARIANT:
@@ -42,7 +61,7 @@ public class ResultsWriterFactory {
             case PHENOGRID:
                 return new PhenogridWriter();
             default:
-                return new HtmlResultsWriter(templateEngine);
+                return new HtmlResultsWriter(coreTemplateEngine);
         }
     }   
 }
