efd83b25ab1bf289f1a8b51a9dcfc632cc2cb859
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-06-06 14:07:35 +0100

    Added equals, hashCode and toString methods to DiseaseModel. Aligned GeneModel.toString() with DiseaseModel.toString()

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/DiseaseModel.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/DiseaseModel.java
@@ -1,4 +1,23 @@
 /*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,6 +27,7 @@ package de.charite.compbio.exomiser.core.model;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  *
@@ -100,5 +120,38 @@ public class DiseaseModel implements Model {
             bestPhenotypeMatchForTerms.put(matchQueryTerm, match);
         } 
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DiseaseModel)) return false;
+        DiseaseModel that = (DiseaseModel) o;
+        return entrezGeneId == that.entrezGeneId &&
+                Objects.equals(modelId, that.modelId) &&
+                organism == that.organism &&
+                Objects.equals(humanGeneSymbol, that.humanGeneSymbol) &&
+                Objects.equals(diseaseId, that.diseaseId) &&
+                Objects.equals(diseaseTerm, that.diseaseTerm) &&
+                Objects.equals(phenotypeIds, that.phenotypeIds);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(modelId, organism, entrezGeneId, humanGeneSymbol, diseaseId, diseaseTerm, phenotypeIds);
+    }
+
+    @Override
+    public String toString() {
+        return "DiseaseModel{" +
+                "modelId='" + modelId + '\'' +
+                ", organism=" + organism +
+                ", entrezGeneId=" + entrezGeneId +
+                ", humanGeneSymbol='" + humanGeneSymbol + '\'' +
+                ", diseaseId='" + diseaseId + '\'' +
+                ", diseaseTerm='" + diseaseTerm + '\'' +
+                ", phenotypeIds=" + phenotypeIds +
+                ", score=" + score +
+                ", bestPhenotypeMatchForTerms=" + bestPhenotypeMatchForTerms +
+                '}';
+    }
 }
--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/GeneModel.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/GeneModel.java
@@ -1,4 +1,23 @@
 /*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -156,7 +175,16 @@ public class GeneModel implements Model {
 
     @Override
     public String toString() {
-        return "GeneModel{score=" + score + ", entrezGeneId=" + entrezGeneId + ", humanGeneSymbol=" + humanGeneSymbol + ", modelId=" + modelId + ", modelSymbol=" + modelGeneSymbol + ", phenotypeIds=" + phenotypeIds + '}';
+        return "GeneModel{" +
+                "modelId='" + modelId + '\'' +
+                ", organism=" + organism +
+                ", entrezGeneId=" + entrezGeneId +
+                ", humanGeneSymbol='" + humanGeneSymbol + '\'' +
+                ", modelGeneId='" + modelGeneId + '\'' +
+                ", modelGeneSymbol='" + modelGeneSymbol + '\'' +
+                ", phenotypeIds=" + phenotypeIds +
+                ", score=" + score +
+                ", bestPhenotypeMatchForTerms=" + bestPhenotypeMatchForTerms +
+                '}';
     }
-    
 }
