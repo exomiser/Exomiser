0930cb8753e34c12354de72a7df812f94cdfa5c7
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-06-10 17:02:28 +0100

    Extracted PPI logic from HiPhivePriority into self-contained HiPhiveProteinInteractionScorer class.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriority.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriority.java
@@ -22,11 +22,9 @@ package de.charite.compbio.exomiser.core.prioritisers;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Lists;
 import de.charite.compbio.exomiser.core.model.*;
-import de.charite.compbio.exomiser.core.prioritisers.util.DataMatrix;
-import de.charite.compbio.exomiser.core.prioritisers.util.OrganismPhenotypeMatches;
-import de.charite.compbio.exomiser.core.prioritisers.util.PriorityService;
-import org.jblas.FloatMatrix;
+import de.charite.compbio.exomiser.core.prioritisers.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,6 +46,7 @@ public class HiPhivePriority implements Prioritiser {
     private static final Logger logger = LoggerFactory.getLogger(HiPhivePriority.class);
 
     private static final PriorityType PRIORITY_TYPE = PriorityType.HIPHIVE_PRIORITY;
+    private static final double HIGH_QUALITY_SCORE_CUTOFF = 0.6;
 
     private final HiPhiveOptions options;
     private final DataMatrix randomWalkMatrix;
@@ -57,9 +56,8 @@ public class HiPhivePriority implements Prioritiser {
 
     //TODO: make local
     private final List<String> hpoIds;
-    private List<Integer> highQualityPhenoMatchedGenes = new ArrayList<>();
-    private Map<Integer, Double> geneScores = new HashMap<>();
 
+    //TODO: make local
     private double bestMaxScore = 0d;
     private double bestAvgScore = 0d;
 
@@ -101,33 +99,25 @@ public class HiPhivePriority implements Prioritiser {
             removeKnownGeneDiseaseAssociationModel(bestGeneModels);
         }
 
-        FloatMatrix weightedHighQualityMatrix = FloatMatrix.EMPTY;
+        HiPhiveProteinInteractionScorer ppiScorer = HiPhiveProteinInteractionScorer.EMPTY;
         if (options.runPpi()) {
-            for (Model model : bestGeneModels.values()) {
-                Integer entrezId = model.getEntrezGeneId();
-                Double score = model.getScore();
-                // normal behaviour when not trying to exclude candidate gene to simulate novel gene discovery in benchmarking
-                // only build PPI network for high qual hits
-                if (score > 0.6) {
-                    logger.debug("Adding high quality score for {} score={}", model.getHumanGeneSymbol(), model.getScore());
-                    //TODO: this is a bit round-the-houses as it's used in getColumnIndexOfMostPhenotypicallySimilarGene() would probably
-                    //be better using a LinkedHashMap to join these two values together, or use a GeneIdentifier class....?
-                    highQualityPhenoMatchedGenes.add(entrezId);
-                    //TODO: geneScores is only ever queried using highQualityPhenoMatchedGenes so the two can be merged so we have the highest score from all models
-                    //linked to a gene id. This is used to find the weighted high-quality matrix.
-                }
-                //why use this? Just return the high-quality gene-model matches and iterate through the fuckers.
-                //also used by makeWeightedProteinInteractionMatrixFromHighQualityPhenotypeMatchedGenes
-                if (!geneScores.containsKey(entrezId) || score > geneScores.get(entrezId)) {
-                    geneScores.put(entrezId, score);
-                }
-            }
-            weightedHighQualityMatrix = makeWeightedProteinInteractionMatrixFromHighQualityPhenotypeMatchedGenes(highQualityPhenoMatchedGenes, geneScores);
+            ppiScorer = new HiPhiveProteinInteractionScorer(randomWalkMatrix, bestGeneModels, HIGH_QUALITY_SCORE_CUTOFF);
         }
 
         logger.info("Prioritising genes...");
         for (Gene gene : genes) {
-            HiPhivePriorityResult priorityResult = makePriorityResultForGene(gene, hpoPhenotypeTerms, bestGeneModels, weightedHighQualityMatrix);
+
+            Integer entrezGeneId = gene.getEntrezGeneID();
+            List<Model> bestPhenotypeMatchModels = bestGeneModels.get(entrezGeneId);
+            double phenoScore = bestPhenotypeMatchModels.stream().mapToDouble(Model::getScore).max().orElse(0);
+
+            GeneMatch closestPhenoMatchInNetwork = ppiScorer.getClosestPhenoMatchInNetwork(entrezGeneId);
+            List<Model> closestPhysicallyInteractingGeneModels = closestPhenoMatchInNetwork.getBestMatchModels();
+            double walkerScore = closestPhenoMatchInNetwork.getScore();
+
+            double score = Double.max(phenoScore, walkerScore);
+            logger.debug("Making result for {} {} score={} phenoScore={} walkerScore={}", gene.getGeneSymbol(), entrezGeneId, score, phenoScore, walkerScore);
+            HiPhivePriorityResult priorityResult = new HiPhivePriorityResult(gene.getGeneSymbol(), score, hpoPhenotypeTerms, bestPhenotypeMatchModels, closestPhysicallyInteractingGeneModels, walkerScore, matchesCandidateGeneSymbol(gene));
             gene.addPriorityResult(priorityResult);
         }
 
@@ -136,14 +126,20 @@ public class HiPhivePriority implements Prioritiser {
     private ListMultimap<Integer, Model> makeBestGeneModelsForOrganisms(List<PhenotypeTerm> hpoPhenotypeTerms, Organism referenceOrganism, Set<Organism> organismsToCompare) {
         ListMultimap<Integer, Model> bestGeneModels = ArrayListMultimap.create();
 
-        //TODO: this is repetitive, surely there must be a better way to deal with these, perhaps a GeneModelMatrix class?
-        final Map<Integer, Model> bestDiseaseModelForGene = makeHpToHumanMatches(options.runHuman(), hpoPhenotypeTerms);
+        //CAUTION!! this must always run in order that the best score is set - HUMAN runs first as we are comparing HP to other phenotype ontology terms.
+        OrganismPhenotypeMatches humanPhenotypeMatches = getMatchingPhenotypesForOrganism(hpoPhenotypeTerms, referenceOrganism);
+        Set<PhenotypeMatch> bestMatches = humanPhenotypeMatches.getBestPhenotypeMatches();
+
+        calculateBestScoresFromHumanPhenotypes(bestMatches, hpoPhenotypeTerms);
+
+        //TODO: this is repetitive, surely there must be a better way to deal with these. the boolean and Species are linked...
+        final Map<Integer, Model> bestDiseaseModelForGene = makeHpToHumanMatches(options.runHuman(), humanPhenotypeMatches, referenceOrganism);
         final Map<Integer, Model> bestMouseModelForGene = makeHpToOtherSpeciesMatches(options.runMouse(), hpoPhenotypeTerms, Organism.MOUSE);
         final Map<Integer, Model> bestFishModelForGene = makeHpToOtherSpeciesMatches(options.runFish(), hpoPhenotypeTerms, Organism.FISH);
 
-        bestDiseaseModelForGene.entrySet().stream().forEach(entry -> bestGeneModels.put(entry.getKey(), entry.getValue()));
-        bestMouseModelForGene.entrySet().stream().forEach(entry -> bestGeneModels.put(entry.getKey(), entry.getValue()));
-        bestFishModelForGene.entrySet().stream().forEach(entry -> bestGeneModels.put(entry.getKey(), entry.getValue()));
+        List<Map<Integer, Model>> bestModels = Lists.newArrayList(bestDiseaseModelForGene, bestMouseModelForGene, bestFishModelForGene);
+        bestModels.forEach(item -> item.entrySet().stream().forEach(entry -> bestGeneModels.put(entry.getKey(), entry.getValue())));
+
         return bestGeneModels;
     }
 
@@ -154,35 +150,6 @@ public class HiPhivePriority implements Prioritiser {
                     bestGeneModels.remove(model.getEntrezGeneId(), model);
                     logger.info("Found benchmarking hit {}:{} - removing model {}", options.getDiseaseId(), options.getCandidateGeneSymbol(), model);
                 });
-    }
-
-    private HiPhivePriorityResult makePriorityResultForGene(Gene gene, List<PhenotypeTerm> hpoPhenotypeTerms, ListMultimap<Integer, Model> bestGeneModels, FloatMatrix weightedHighQualityMatrix) {
-
-        Integer entrezGeneId = gene.getEntrezGeneID();
-        List<Model> bestPhenotypeMatchModels = bestGeneModels.get(entrezGeneId);
-        double score = 0;
-        for (Model model : bestPhenotypeMatchModels) {
-            score = Math.max(score, model.getScore());
-        }
-
-        List<Model> closestPhysicallyInteractingGeneModels = new ArrayList<>();
-        double walkerScore = 0d;
-        if (options.runPpi() && randomWalkMatrix.containsGene(entrezGeneId) && !highQualityPhenoMatchedGenes.isEmpty()) {
-            Integer columnIndex = getColumnIndexOfMostPhenotypicallySimilarGene(entrezGeneId, highQualityPhenoMatchedGenes, weightedHighQualityMatrix);
-            Integer rowIndex = randomWalkMatrix.getRowIndexForGene(entrezGeneId);
-            walkerScore = weightedHighQualityMatrix.get(rowIndex, columnIndex);
-            // optimal adjustment based on benchmarking to allow walker scores to compete with low phenotype scores
-            walkerScore = 0.5 +  walkerScore;
-            score = Math.max(score, walkerScore);
-            if (walkerScore <= 0.00001) {
-                walkerScore = 0d;
-            } else {
-                Integer closestGeneId = highQualityPhenoMatchedGenes.get(columnIndex);
-                closestPhysicallyInteractingGeneModels = bestGeneModels.get(closestGeneId);
-            }
-        }
-        logger.debug("Making result for {} {} score={}", gene.getGeneSymbol(), entrezGeneId, score);
-        return new HiPhivePriorityResult(gene.getGeneSymbol(), score, hpoPhenotypeTerms, bestPhenotypeMatchModels, closestPhysicallyInteractingGeneModels, walkerScore, matchesCandidateGeneSymbol(gene));
     }
 
     private boolean matchesCandidateGeneSymbol(Gene gene) {
@@ -203,16 +170,9 @@ public class HiPhivePriority implements Prioritiser {
                 numGenesWithPhenotypeOrPpiData, totalGenes, 100f * (numGenesWithPhenotypeOrPpiData / (float) totalGenes));
     }
 
-    private Map<Integer, Model> makeHpToHumanMatches(boolean runHuman, List<PhenotypeTerm> queryHpoPhenotypes) {
-        //TODO: this must always run in order that the best score is set 
-        // Human
-        OrganismPhenotypeMatches humanPhenotypeMatches = getMatchingPhenotypesForOrganism(queryHpoPhenotypes, Organism.HUMAN);
-        Set<PhenotypeMatch> bestMatches = humanPhenotypeMatches.getBestPhenotypeMatches();
-
-        calculateBestScoresFromHumanPhenotypes(bestMatches, queryHpoPhenotypes);
-
+    private Map<Integer, Model> makeHpToHumanMatches(boolean runHuman, OrganismPhenotypeMatches humanPhenotypeMatches, Organism organism) {
         if (runHuman) {
-            List<Model> organismModels = priorityService.getModelsForOrganism(Organism.HUMAN);
+            List<Model> organismModels = priorityService.getModelsForOrganism(organism);
             Map<Integer, Set<Model>> geneModelPhenotypeMatches = calculateGeneModelPhenotypeMatches(humanPhenotypeMatches, organismModels);
             return makeBestGeneModelForGenes(geneModelPhenotypeMatches);
         } else {
@@ -231,14 +191,23 @@ public class HiPhivePriority implements Prioritiser {
         }
     }
 
-    private OrganismPhenotypeMatches getMatchingPhenotypesForOrganism(List<PhenotypeTerm> queryHpoPhenotypes, Organism species) {
-        logger.info("Fetching HUMAN-{} phenotype matches...", species);
+    private OrganismPhenotypeMatches getMatchingPhenotypesForOrganism(List<PhenotypeTerm> queryHpoPhenotypes, Organism organism) {
+        logger.info("Fetching HUMAN-{} phenotype matches...", organism);
         Map<PhenotypeTerm, Set<PhenotypeMatch>> speciesPhenotypeMatches = new LinkedHashMap<>();
         for (PhenotypeTerm hpoTerm : queryHpoPhenotypes) {
-            Set<PhenotypeMatch> termMatches = priorityService.getSpeciesMatchesForHpoTerm(hpoTerm, species);
+            Set<PhenotypeMatch> termMatches = priorityService.getSpeciesMatchesForHpoTerm(hpoTerm, organism);
             speciesPhenotypeMatches.put(hpoTerm, termMatches);
         }
-        return new OrganismPhenotypeMatches(species, ImmutableMap.copyOf(speciesPhenotypeMatches));
+
+        OrganismPhenotypeMatches organismPhenotypeMatches = new OrganismPhenotypeMatches(organism, ImmutableMap.copyOf(speciesPhenotypeMatches));
+
+        logger.info("Best {} phenotype matches:", organism);
+        for (PhenotypeMatch bestMatch : organismPhenotypeMatches.getBestPhenotypeMatches()) {
+            logger.info("{}-{}={}", bestMatch.getQueryPhenotypeId(), bestMatch.getMatchPhenotypeId(), bestMatch.getScore());
+        }
+        logger.info("bestMaxScore={} bestAvgScore={}", organismPhenotypeMatches.getBestMatchScore(), organismPhenotypeMatches.getBestAverageScore(), organismPhenotypeMatches.getBestPhenotypeMatches().size());
+
+        return organismPhenotypeMatches;
     }
 
 //    class BestScores
@@ -257,7 +226,7 @@ public class HiPhivePriority implements Prioritiser {
             bestMaxScore = Math.max(matchScore, bestMaxScore);
             sumBestScore += matchScore;
         }
-        if (bestMatches.size() > 0){// otherwise get a NaN value that escalates to other scores and eventually throws an exception
+        if (!bestMatches.isEmpty()){// otherwise get a NaN value that escalates to other scores and eventually throws an exception
             //bestAvgScore = sumBestScore / bestMatches.size();
             // Aug2015 - average across all HPOs to be consistent with change in calculateBestGeneModelPhenotypeMatchForSpecies
             bestAvgScore = sumBestScore / queryHpoPhenotypes.size();
@@ -399,56 +368,6 @@ public class HiPhivePriority implements Prioritiser {
         return bestGeneModelForGenes;
     }
 
-    //todo: If this returned a DataMatrix things might be a bit more convienent later on... 
-    //TODO: does this have to be a list and a map? Can't it just be a map of scores?
-    private FloatMatrix makeWeightedProteinInteractionMatrixFromHighQualityPhenotypeMatchedGenes(List<Integer> highQualityPhenoMatchedGenes, Map<Integer, Double> geneScores) {
-        logger.info("Making weighted-score Protein-Protein interaction sub-matrix from high quality phenotypic gene matches...");
-        int rows = randomWalkMatrix.getMatrix().getRows();
-        int cols = highQualityPhenoMatchedGenes.size();
-        FloatMatrix highQualityPpiMatrix = FloatMatrix.zeros(rows, cols);
-        int c = 0;
-        for (Integer seedGeneEntrezId : highQualityPhenoMatchedGenes) {
-            if (randomWalkMatrix.containsGene(seedGeneEntrezId)) {
-                FloatMatrix column = randomWalkMatrix.getColumnMatrixForGene(seedGeneEntrezId);
-                // weight column by phenoScore 
-                Double score = geneScores.get(seedGeneEntrezId);
-                column = column.mul(score.floatValue());
-                highQualityPpiMatrix.putColumn(c, column);
-            }
-            c++;
-        }
-        return highQualityPpiMatrix;
-    }
-
-    /**
-     * This function retrieves the random walk similarity score for the gene
-     *
-     * @param entrezGeneId for which the random walk score is to be retrieved
-     */
-    private int getColumnIndexOfMostPhenotypicallySimilarGene(int entrezGeneId, List<Integer> phenotypicallySimilarGeneIds, FloatMatrix weightedHighQualityMatrix) {
-        int geneIndex = randomWalkMatrix.getRowIndexForGene(entrezGeneId);
-        int columnIndex = 0;
-        double bestScore = 0;
-        int bestHitIndex = 0;
-        for (Integer similarGeneEntrezId : phenotypicallySimilarGeneIds) {
-            if (!randomWalkMatrix.containsGene(similarGeneEntrezId)) {
-                columnIndex++;
-                continue;
-            } else if (similarGeneEntrezId == entrezGeneId) {
-                //avoid self-hits now are testing genes with direct pheno-evidence as well
-                columnIndex++;
-                continue;
-            } else {
-                double cellScore = weightedHighQualityMatrix.get(geneIndex, columnIndex);
-                if (cellScore > bestScore) {
-                    bestScore = cellScore;
-                    bestHitIndex = columnIndex;
-                }
-                columnIndex++;
-            }
-        }
-        return bestHitIndex;
-    }
 
     public void setPriorityService(PriorityService priorityService) {
         this.priorityService = priorityService;
--- exomiser-core/src/test/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriorityTest.java
+++ exomiser-core/src/test/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriorityTest.java
@@ -1,20 +1,45 @@
 /*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package de.charite.compbio.exomiser.core.prioritisers;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
 import de.charite.compbio.exomiser.core.model.Gene;
+import de.charite.compbio.exomiser.core.prioritisers.util.DataMatrix;
+import org.jblas.DoubleMatrix;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  *
@@ -32,16 +57,57 @@ public class HiPhivePriorityTest {
     @Before
     public void setUp() {
         hpoIds = new ArrayList<>();
-        hpoIds.add("HP:000001");
-        hpoIds.add("HP:000002");
-        hpoIds.add("HP:000003");
-        
-        candidateGene = "GENE1";
-        disease = "OMIM:100100";
+        hpoIds.add("HP:0010055");
+        hpoIds.add("HP:0001363");
+        hpoIds.add("HP:0001156");
+        hpoIds.add("HP:0011304");
+
+        candidateGene = "FGFR2";
+        disease = "OMIM:101600";
         exomiser2params = "";
-        
-        instance = new HiPhivePriority(hpoIds, new HiPhiveOptions(), null);
+
+        double[][] ppiMatrix = {
+                {0.707653999329, 0.000000009625, 0.000000008875, 0.000000372898, 0.000000268611, 0.000000023074, 0.000000040680, 0.000000133227, 0.000000064774, 0.000000113817},
+                {0.000000005477, 0.713751792908, 0.000008168789, 0.000000000210, 0.000000001862, 0.000000013144, 0.000000000679, 0.000000001696, 0.000000001134, 0.000000002901},
+                {0.000000032869, 0.000053159707, 0.703132450581, 0.000000005321, 0.000001411398, 0.000000034059, 0.000000138069, 0.000000080616, 0.000000016616, 0.000001185472},
+                {0.000001284835, 0.000000001273, 0.000000004950, 0.701313674450, 0.000000023567, 0.000000434552, 0.000001564053, 0.000000178326, 0.000000213718, 0.000000099803},
+                {0.000000758085, 0.000000009233, 0.000001075537, 0.000000019304, 0.702176809311, 0.000000022661, 0.000000009667, 0.000000021993, 0.000000019951, 0.000000053246},
+                {0.000000047331, 0.000000047378, 0.000000018865, 0.000000258712, 0.000000016471, 0.707689404488, 0.000006197341, 0.000000126649, 0.000000020872, 0.000000080030},
+                {0.000000023518, 0.000000000690, 0.000000021553, 0.000000262441, 0.000000001980, 0.000001746652, 0.702001333237, 0.000000298203, 0.000000004386, 0.000000007073},
+                {0.000000442888, 0.000000009907, 0.000000072361, 0.000000172052, 0.000000025906, 0.000000205243, 0.000001714670, 0.700654745102, 0.000000116321, 0.000000084246},
+                {0.000000010099, 0.000000000311, 0.000000000700, 0.000000009671, 0.000000001102, 0.000000001586, 0.000000001183, 0.000000005456, 0.701215505600, 0.000000269338},
+                {0.000000034608, 0.000000001550, 0.000000097330, 0.000000008808, 0.000000005737, 0.000000011863, 0.000000003720, 0.000000007706, 0.000000525256, 0.705851793289}
+        };
+        DoubleMatrix doubleMatrix = new DoubleMatrix(ppiMatrix);
+
+        Arrays.stream(doubleMatrix.getRow(5).toArray()).forEach(val -> System.out.printf("%.12f, ", val));
+        System.out.println();
+        Arrays.stream(doubleMatrix.getColumn(5).toArray()).forEach(val -> System.out.printf("%.12f, ", val));
+        System.out.println();
+
+
+        DataMatrix testMatrix = new DataMatrix(doubleMatrix.toFloat(), new HashMap<>());
+
+        instance = new HiPhivePriority(hpoIds, new HiPhiveOptions(), testMatrix);
+//        instance.setPriorityService();
+
+//        HP:0010055-HP:0010055=2.7796420474783035
+//        HP:0001363-HP:0001363=2.4336466530917122
+//        HP:0001156-HP:0001156=1.9399600254934881
+//        HP:0011304-HP:0011304=2.7485534157741984
+
+//        HP:0010055-MP:0009049=2.530139606718946
+//        HP:0001363-MP:0000081=2.6914281512491542
+//        HP:0001156-MP:0002544=2.2666863877455286
+//        HP:0011304-MP:0002543=2.170374978266971
+
+//        HP:0010055-ZP:0012193=1.0920661382591106
+//        HP:0001363-ZP:0003564=1.9560677583289208
+//        HP:0001156-ZP:0012193=1.1921761629223673
+//        HP:0011304-ZP:0001082=1.1965410868294204
     }
+
+
 
     @Test
     public void testGetPriorityType() {
@@ -62,8 +128,22 @@ public class HiPhivePriorityTest {
         instance.prioritizeGenes(new ArrayList<Gene>());
     }
 
+    @Ignore
     @Test
     public void testSetPriorityService() {
+        ArrayListMultimap<Integer, Double>  multimapInsertOrderTest = ArrayListMultimap.create();
+        multimapInsertOrderTest.put(1, 1.0);
+        multimapInsertOrderTest.put(1, 2.0);
+        multimapInsertOrderTest.put(1, 3.0);
+
+        multimapInsertOrderTest.put(3, 1.0);
+
+        multimapInsertOrderTest.put(2, 1.0);
+        multimapInsertOrderTest.put(2, 1.0);
+
+        System.out.println(multimapInsertOrderTest);
+        List<Integer> expectedOrder = Lists.newArrayList(1, 3, 2);
+        assertThat(multimapInsertOrderTest.keySet(), equalTo(expectedOrder));
     }
 
     @Test
