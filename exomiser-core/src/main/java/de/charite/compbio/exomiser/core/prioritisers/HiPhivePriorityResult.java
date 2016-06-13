204fb383268a71726947dad3823ea44110f11cee
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-06-13 10:47:47 +0100

    Removed unused HiPhivePriorityResult.setScore() method.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriorityResult.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriorityResult.java
@@ -1,9 +1,29 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.charite.compbio.exomiser.core.prioritisers;
 
 import de.charite.compbio.exomiser.core.model.DiseaseModel;
 import de.charite.compbio.exomiser.core.model.Model;
 import de.charite.compbio.exomiser.core.model.PhenotypeMatch;
 import de.charite.compbio.exomiser.core.model.PhenotypeTerm;
+
 import java.util.List;
 import java.util.Map;
 
@@ -73,10 +93,6 @@ public class HiPhivePriorityResult implements PriorityResult {
     @Override
     public float getScore() {
         return (float) score;
-    }
-
-    public void setScore(double newScore) {
-        this.score = newScore;
     }
 
     public List<PhenotypeTerm> getQueryPhenotypeTerms() {
--- exomiser-core/src/test/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriorityResultTest.java
+++ exomiser-core/src/test/java/de/charite/compbio/exomiser/core/prioritisers/HiPhivePriorityResultTest.java
@@ -1,4 +1,23 @@
 /*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,15 +26,17 @@ package de.charite.compbio.exomiser.core.prioritisers;
 
 import de.charite.compbio.exomiser.core.model.GeneModel;
 import de.charite.compbio.exomiser.core.model.Model;
-import de.charite.compbio.exomiser.core.model.PhenotypeTerm;
 import de.charite.compbio.exomiser.core.model.Organism;
-import java.util.ArrayList;
-import java.util.List;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import de.charite.compbio.exomiser.core.model.PhenotypeTerm;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  *
@@ -59,13 +80,6 @@ public class HiPhivePriorityResultTest {
     @Test
     public void testGetScore() {
         assertThat(instance.getScore(), equalTo((float) score));
-    }
-
-    @Test
-    public void testSetScore() {
-        double newScore = 1.0d;
-        instance.setScore(newScore);
-        assertThat(instance.getScore(), equalTo((float) newScore));
     }
 
     @Test
