838297b6486947ad45b3559239fbbbc9652463b2
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-09-15 17:40:51 +0100

    Correcting erroneous mutability of public static fields in FrequencySource.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/frequency/FrequencySource.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/frequency/FrequencySource.java
@@ -1,8 +1,29 @@
 /*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package de.charite.compbio.exomiser.core.model.frequency;
+
+import com.google.common.collect.Sets;
 
 import java.util.EnumSet;
 import java.util.Set;
@@ -36,11 +57,11 @@ public enum FrequencySource {
     EXAC_NON_FINNISH_EUROPEAN("ExAC NFE"),
     EXAC_OTHER("ExAC OTH");
     
-    public static final Set<FrequencySource> ALL_ESP_SOURCES = EnumSet.range(ESP_AFRICAN_AMERICAN, ESP_ALL);
+    public static final Set<FrequencySource> ALL_ESP_SOURCES = Sets.immutableEnumSet(EnumSet.range(ESP_AFRICAN_AMERICAN, ESP_ALL));
 
-    public static final Set<FrequencySource> ALL_EXAC_SOURCES = EnumSet.range(EXAC_AFRICAN_INC_AFRICAN_AMERICAN, EXAC_OTHER);
+    public static final Set<FrequencySource> ALL_EXAC_SOURCES = Sets.immutableEnumSet(EnumSet.range(EXAC_AFRICAN_INC_AFRICAN_AMERICAN, EXAC_OTHER));
     
-    public static final Set<FrequencySource> ALL_EXTERNAL_FREQ_SOURCES = EnumSet.range(THOUSAND_GENOMES, EXAC_OTHER);
+    public static final Set<FrequencySource> ALL_EXTERNAL_FREQ_SOURCES = Sets.immutableEnumSet(EnumSet.range(THOUSAND_GENOMES, EXAC_OTHER));
         
     private final String source;
             
