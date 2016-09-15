391f4a666a338a51b1048c53d3ee03ef97f40117
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-09-15 10:56:14 +0100

    Fixed control flow duplication in DiseaseModel

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/DiseaseModel.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/DiseaseModel.java
@@ -114,11 +114,9 @@ public class DiseaseModel implements Model {
     @Override
     public void addMatchIfAbsentOrBetterThanCurrent(PhenotypeMatch match) {
         PhenotypeTerm matchQueryTerm = match.getQueryPhenotype();
-        if (!bestPhenotypeMatchForTerms.containsKey(matchQueryTerm)) {
+        if (!bestPhenotypeMatchForTerms.containsKey(matchQueryTerm) || (bestPhenotypeMatchForTerms.get(matchQueryTerm).getScore() < match.getScore())) {
             bestPhenotypeMatchForTerms.put(matchQueryTerm, match);
-        } else if (bestPhenotypeMatchForTerms.get(matchQueryTerm).getScore() < match.getScore()) {
-            bestPhenotypeMatchForTerms.put(matchQueryTerm, match);
-        } 
+        }
     }
 
     @Override
