6f1002ffea2edc338e725ac522a0cc854aa7d0fc
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2016-09-19 10:48:10 +0100

    VCF2FrequencyParser now throws a dedicated exception instead of calling System.exit.

--- exomiser-db/src/main/java/de/charite/compbio/exomiser/db/parsers/VCF2FrequencyParser.java
+++ exomiser-db/src/main/java/de/charite/compbio/exomiser/db/parsers/VCF2FrequencyParser.java
@@ -1,13 +1,33 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize variants
+ *
+ * Copyright (C) 2012 - 2016  Charite Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.charite.compbio.exomiser.db.parsers;
 
 import de.charite.compbio.exomiser.db.reference.Frequency;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 import de.charite.compbio.jannovar.data.ReferenceDictionary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * We are parsing two different VCF files for population frequency data on
@@ -66,9 +86,9 @@ public class VCF2FrequencyParser {
         try {
             chrom = (byte) refDict.getContigNameToID().get(fields[0]).intValue();
         } catch (NumberFormatException e) {
-            logger.error("Unable to parse chromosome: {}. Error occured parsing line: {}", fields[0], line);
-            logger.error("", e.getMessage());
-            System.exit(1);
+            String message = String.format("Unable to parse chromosome: %s. Error occured parsing line: %s", fields[0], line);
+            logger.error(message, e.getMessage());
+            throw new ResourceParserException(message, e);
         }
         if (chrom != chromosome){
             return frequencyList;
