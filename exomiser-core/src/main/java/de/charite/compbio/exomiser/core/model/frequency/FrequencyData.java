97892961e329d79d5b81a2812980b0fc3353cc97
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2016-09-19 13:33:41 +0100

    Removed copyright template comments.

--- exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/frequency/FrequencyData.java
+++ exomiser-core/src/main/java/de/charite/compbio/exomiser/core/model/frequency/FrequencyData.java
@@ -17,11 +17,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.charite.compbio.exomiser.core.model.frequency;
 
 import org.slf4j.Logger;
