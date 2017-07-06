76a40611832f0602085576f35ebdd866a5d60460
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2017-07-06 14:14:22 +0100

    Added test cases for LocalFrequencyDao covering non-autosomal chromosomal positions.

--- exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/genome/dao/LocalFrequencyDaoTest.java
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/genome/dao/LocalFrequencyDaoTest.java
@@ -50,6 +50,10 @@ public class LocalFrequencyDaoTest {
     //1 12345   A   TG   0.01  (an A->TG insertion on chr1 at position 12345 with frequency of 0.01%)
     //1 12345   AT   G   0.02  (an AT->G deletion on chr1 at position 12345 with frequency of 0.02%)
     //1 12345   T   .   0.03  (an T->. monomorphic site (no alt allele) on chr1 at position 12345 with frequency of 0.03%)
+    //non-autosomes
+    //X 12345   AT   G   0.02  (an AT->G deletion on chrX at position 12345 with frequency of 0.02%)
+    //Y 12345   AT   G   0.02  (an AT->G deletion on chrY at position 12345 with frequency of 0.02%)
+    //M 12345   AT   G   0.02  (an AT->G deletion on chrM at position 12345 with frequency of 0.02%)
 
     @Test
     public void variantNotInFile() {
@@ -60,12 +64,36 @@ public class LocalFrequencyDaoTest {
     }
 
     @Test
-    public void testSnp(){
+    public void testAutosomalSnp() {
         //1 12345   A   T   23.0  (an A->T SNP on chr1 at position 12345 with frequency of 23.0%)
         Mockito.when(tabixReader.query("1:12345-12345"))
                 .thenReturn(new MockTabixIterator(Collections.singletonList("1\t12345\tA\tT\t23.0")));
 
         assertThat(instance.getFrequencyData(variant(1, 12345, "A", "T")), equalTo(localFrequencyData(23.0f)));
+    }
+
+    @Test
+    public void testSexXsnp() {
+        Mockito.when(tabixReader.query("X:12345-12345"))
+                .thenReturn(new MockTabixIterator(Collections.singletonList("X\t12345\tA\tT\t23.0")));
+
+        assertThat(instance.getFrequencyData(variant(23, 12345, "A", "T")), equalTo(localFrequencyData(23.0f)));
+    }
+
+    @Test
+    public void testSexYsnp() {
+        Mockito.when(tabixReader.query("Y:12345-12345"))
+                .thenReturn(new MockTabixIterator(Collections.singletonList("Y\t12345\tA\tT\t23.0")));
+
+        assertThat(instance.getFrequencyData(variant(24, 12345, "A", "T")), equalTo(localFrequencyData(23.0f)));
+    }
+
+    @Test
+    public void testMitochondrialSnp() {
+        Mockito.when(tabixReader.query("M:12345-12345"))
+                .thenReturn(new MockTabixIterator(Collections.singletonList("M\t12345\tA\tT\t23.0")));
+
+        assertThat(instance.getFrequencyData(variant(25, 12345, "A", "T")), equalTo(localFrequencyData(23.0f)));
     }
 
     @Test
