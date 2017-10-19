a72c4ca838c111070fd3ce7f362c17231abaf675
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2017-10-19 17:16:26 +0100

    Add cache configuration to exomiser-cli

--- exomiser-cli/src/main/java/org/monarchinitiative/exomiser/cli/config/MainConfig.java
+++ exomiser-cli/src/main/java/org/monarchinitiative/exomiser/cli/config/MainConfig.java
@@ -21,13 +21,17 @@
 package org.monarchinitiative.exomiser.cli.config;
 
 import org.monarchinitiative.exomiser.autoconfigure.EnableExomiser;
+import org.monarchinitiative.exomiser.autoconfigure.ExomiserAutoConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.PathResource;
@@ -40,6 +44,9 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -94,6 +101,51 @@ public class MainConfig {
         Path dataPath = jarFilePath.resolve(dataDirValue);
         logger.info("Root data source directory set to: {}", dataPath.toAbsolutePath());
         return dataPath;
+    }
+
+    @Bean
+    @ConditionalOnProperty("exomiser.cache")
+    public CacheManager cacheManager() {
+        String cacheOption = env.getProperty("exomiser.cache");
+        //see http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html for how this works
+        CacheManager cacheManager;
+        List<String> cacheNames = new ArrayList<>();
+        switch (cacheOption) {
+            case "none":
+                logger.info("Caching disabled.");
+                cacheManager = new NoOpCacheManager();
+                break;
+            case "mem":
+                logger.info("Using unbounded memory cache.");
+                cacheManager = new ConcurrentMapCacheManager();
+                break;
+            case "ehcache":
+                logger.info("Using Ehcache.");
+                cacheManager = ehCacheCacheManager();
+                cacheNames.addAll(Arrays.asList(ehCacheCacheManager().getCacheManager().getCacheNames()));
+                break;
+            default:
+                String message = String.format("Unrecognised value '%s' for exomiser cache option. Please choose 'none', 'mem' or 'ehcache'.", cacheOption);
+                logger.error(message);
+                throw new ExomiserAutoConfigurationException(message);
+        }
+        logger.info("Set up {} caches: {}", cacheOption, cacheNames);
+        return cacheManager;
+    }
+
+    private EhCacheCacheManager ehCacheCacheManager() {
+        return new EhCacheCacheManager(ehCacheManager().getObject());
+    }
+
+    @Lazy
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManager() {
+        Resource ehCacheConfig = ehCacheConfig(jarFilePath());
+        logger.info("Loading ehcache.xml from {}", ehCacheConfig.getDescription());
+
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(ehCacheConfig);
+        return ehCacheManagerFactoryBean;
     }
 
     @Bean
