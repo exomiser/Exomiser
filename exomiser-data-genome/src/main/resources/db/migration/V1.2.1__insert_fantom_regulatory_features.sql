5ba5bc3f5037d135be41bd73eb0492e48901cddc
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2017-12-01 13:04:25 +0000

    Add static tad and fantom_permissive_enhancer data for hg19 Add one-off converters for producing resources/genome data for hg19 and hg38 assemblies Add EnsemblEnhancerParser Add FantomEnhancerParser Update resources/db/migration/V1.2.1__insert_fantom_regulatory_features.sql to import new file

--- exomiser-data-genome/src/main/resources/db/migration/V1.2.1__insert_fantom_regulatory_features.sql
+++ exomiser-data-genome/src/main/resources/db/migration/V1.2.1__insert_fantom_regulatory_features.sql
@@ -1,3 +1,3 @@
 INSERT INTO regulatory_regions SELECT *
-                               FROM CSVREAD('${import.path}/fantom.pg', 'chromosome|start|end|feature_type',
+                               FROM CSVREAD('${import.path}/fantom_enhancers.pg', 'chromosome|start|end|feature_type',
                                             'charset=UTF-8 fieldDelimiter='' fieldSeparator=| nullString=NULL');
