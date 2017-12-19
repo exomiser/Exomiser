66d835b91a3ec9a389a7cc77be8930959adefb6c
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2017-12-19 15:29:59 +0000

    Remove superfluous @EnableExomiser from ExomiserWebApp

--- exomiser-web/src/main/java/org/monarchinitiative/exomiser/web/ExomiserWebApp.java
+++ exomiser-web/src/main/java/org/monarchinitiative/exomiser/web/ExomiserWebApp.java
@@ -20,14 +20,12 @@
 
 package org.monarchinitiative.exomiser.web;
 
-import org.monarchinitiative.exomiser.autoconfigure.EnableExomiser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
  */
-@EnableExomiser
 @SpringBootApplication
 public class ExomiserWebApp {
 
