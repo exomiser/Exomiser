e7a8177321fa1848425be76b5f67e569847edf74
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-02-20 16:35:15 +0000

    Add missing <> to Map initialiser in DataMatrixIO

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/util/DataMatrixIO.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/util/DataMatrixIO.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -51,7 +51,7 @@ public class DataMatrixIO {
     }
 
     private static Map<Integer, Integer> createIndex(String object2idxFileZip) {
-        Map<Integer, Integer> index = new HashMap();
+        Map<Integer, Integer> index = new HashMap<>();
         File indexFile = new File(object2idxFileZip);
         try (BufferedReader indexReader = gzippedFileBufferedReader(indexFile)) {
             String line;
