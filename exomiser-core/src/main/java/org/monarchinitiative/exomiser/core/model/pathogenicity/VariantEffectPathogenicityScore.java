934d52008a2b49c42fde2203e1c345e9502fece5
damiansmedley <1Fuzzy234>
2018-03-05 13:31:24 +0000

    Adjusted the default scores for LOF variants to 1 throughout

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/pathogenicity/VariantEffectPathogenicityScore.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/pathogenicity/VariantEffectPathogenicityScore.java
@@ -50,7 +50,7 @@ public abstract class VariantEffectPathogenicityScore {
     /**
      * Assumed pathogenicity score for a frameshift mutation.
      */
-    public static final float FRAMESHIFT_SCORE = 0.95f;
+    public static final float FRAMESHIFT_SCORE = 1.00f;
     /**
      * Assumed pathogenicity score for a nonframeshift indel mutation.
      */
@@ -58,11 +58,11 @@ public abstract class VariantEffectPathogenicityScore {
     /**
      * Assumed pathogenicity score for a nonsense mutation.
      */
-    public static final float NONSENSE_SCORE = 0.95f;
+    public static final float NONSENSE_SCORE = 1.00f;
     /**
      * Assumed pathogenicity score for a splice site mutation.
      */
-    public static final float SPLICING_SCORE = 0.90f;
+    public static final float SPLICING_SCORE = 1.00f;
     /**
      * Assumed pathogenicity score for a synonymous mutation.
      */
@@ -70,12 +70,12 @@ public abstract class VariantEffectPathogenicityScore {
     /**
      * Assumed pathogenicity score for a stoploss mutation.
      */
-    public static final float STOPLOSS_SCORE = 0.70f;
+    public static final float STOPLOSS_SCORE = 1.00f;
     /**
      * Assumed pathogenicity score for a variant that causes the start codon to
      * be lost.
      */
-    public static final float STARTLOSS_SCORE = 0.95f;
+    public static final float STARTLOSS_SCORE = 1.00f;
 
     public static final float getPathogenicityScoreOf(VariantEffect variantEffect) {
         if (variantEffect == VariantEffect.SEQUENCE_VARIANT) {
