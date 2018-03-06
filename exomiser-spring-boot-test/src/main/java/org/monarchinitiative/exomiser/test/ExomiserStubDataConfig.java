722f9373c35a44f063b57a97d7bf304436a6a8ba
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-03-06 22:15:00 +0000

    Add missing stub Beans to ExomiserStubDataConfig

--- exomiser-spring-boot-test/src/main/java/org/monarchinitiative/exomiser/test/ExomiserStubDataConfig.java
+++ exomiser-spring-boot-test/src/main/java/org/monarchinitiative/exomiser/test/ExomiserStubDataConfig.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -27,10 +27,14 @@ import de.charite.compbio.jannovar.reference.HG19RefDictBuilder;
 import htsjdk.tribble.readers.TabixReader;
 import org.mockito.Mockito;
 import org.monarchinitiative.exomiser.core.genome.*;
+import org.monarchinitiative.exomiser.core.prioritisers.util.DataMatrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Configuration to provide a stub classes for exomiser beans which require on-disk files to operate on.
@@ -93,4 +97,13 @@ public class ExomiserStubDataConfig {
         return Mockito.mock(TabixReader.class);
     }
 
+    @Bean
+    public DataMatrix dataMatrix() {
+        return DataMatrix.empty();
+    }
+
+    @Bean
+    Path phenixDataDirectory() {
+        return Paths.get("phenix");
+    }
 }
