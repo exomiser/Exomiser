776cfbe5ca3b79fb9b3a68054d5f3db5dae25492
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-04-03 15:24:57 +0100

    Replace stream with faster loop in PassOnlyAnalysisRunner Simplify stream().forEach() with forEach() in SimpleAnalysisRunner

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/PassOnlyAnalysisRunner.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/PassOnlyAnalysisRunner.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -66,10 +66,11 @@ class PassOnlyAnalysisRunner extends AbstractAnalysisRunner {
     protected Predicate<VariantEvaluation> runVariantFilters(List<VariantFilter> variantFilters) {
         return variantEvaluation -> {
             //loop through the filters and only run if the variantEvaluation has passed all prior filters
-            variantFilters.stream()
-                    .filter(filter -> variantEvaluation.passedFilters())
-                    .forEach(filter -> variantFilterRunner.run(filter, variantEvaluation));
-
+            for (VariantFilter filter : variantFilters) {
+                if (variantEvaluation.passedFilters()) {
+                    variantFilterRunner.run(filter, variantEvaluation);
+                }
+            }
             return variantEvaluation.passedFilters();
         };
     }
--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/SimpleAnalysisRunner.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/SimpleAnalysisRunner.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -51,7 +51,7 @@ class SimpleAnalysisRunner extends AbstractAnalysisRunner {
     protected Predicate<VariantEvaluation> runVariantFilters(List<VariantFilter> variantFilters) {
         return variantEvaluation -> {
             //loop through the filters and run them over the variantEvaluation according to the variantFilterRunner behaviour
-            variantFilters.stream().forEach(filter -> variantFilterRunner.run(filter, variantEvaluation));
+            variantFilters.forEach(filter -> variantFilterRunner.run(filter, variantEvaluation));
             return true;
         };
     }
