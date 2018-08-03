3ebd96cc028af46ad61ef221b4c6180aadc61fbf
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-08-03 12:55:35 +0100

    Oops. Weird Spring usage in data-phenotype. Reverting...

--- exomiser-data-phenotype/src/main/java/org/monarchinitiative/exomiser/data/phenotype/config/DataSourceConfig.java
+++ exomiser-data-phenotype/src/main/java/org/monarchinitiative/exomiser/data/phenotype/config/DataSourceConfig.java
@@ -28,6 +28,7 @@ package org.monarchinitiative.exomiser.data.phenotype.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -49,9 +50,12 @@ public class DataSourceConfig {
 
     private final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
 
+    @Autowired
+    Environment env;
+
     @Primary
     @Bean
-    public DataSource exomiserH2DataSource(Environment env) {
+    public DataSource exomiserH2DataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(env.getProperty("exomiser.h2.driverClassName"));
         dataSource.setUrl(env.getProperty("exomiser.h2.url"));
