329a15ff330484fcd579f4aa4380efcc391dac52
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-11-12 16:14:20 +0000

    Add Variant.isCodingVariant() method

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/Variant.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/Variant.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -49,4 +49,7 @@ public interface Variant extends VariantCoordinates {
         return VariantEffectUtility.isNonCodingVariant(getVariantEffect());
     }
 
+    public default boolean isCodingVariant() {
+        return VariantEffectUtility.affectsCodingRegion(getVariantEffect());
+    }
 }
