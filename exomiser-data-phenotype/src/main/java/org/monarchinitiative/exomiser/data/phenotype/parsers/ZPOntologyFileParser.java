de4f8e9a190b54cb3eefea4cdf5c3492193b3414
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-12-04 12:03:33 +0000

    Remove unnecessary close statements from ZPOntologyFileParser

--- exomiser-data-phenotype/src/main/java/org/monarchinitiative/exomiser/data/phenotype/parsers/ZPOntologyFileParser.java
+++ exomiser-data-phenotype/src/main/java/org/monarchinitiative/exomiser/data/phenotype/parsers/ZPOntologyFileParser.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2018 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -84,8 +84,6 @@ public class ZPOntologyFileParser implements ResourceParser {
                     writer.newLine();
                 }
             }
-            writer.close();
-            reader.close();
             status = ResourceOperationStatus.SUCCESS;
         } catch (FileNotFoundException ex) {
             logger.error(null, ex);
