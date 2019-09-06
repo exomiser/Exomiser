512745f534edaa45019bbbf360058426201c2443
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2019-09-06 13:25:25 +0100

    Refactor TestPriorityServiceFactory to dynamically generate gene and phenotype info

--- exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/prioritisers/service/TestPriorityServiceFactory.java
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/prioritisers/service/TestPriorityServiceFactory.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2017 Queen Mary University of London.
+ * Copyright (c) 2016-2019 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,6 +20,7 @@
 
 package org.monarchinitiative.exomiser.core.prioritisers.service;
 
+import org.monarchinitiative.exomiser.core.model.GeneIdentifier;
 import org.monarchinitiative.exomiser.core.phenotype.PhenotypeMatch;
 import org.monarchinitiative.exomiser.core.phenotype.PhenotypeMatchService;
 import org.monarchinitiative.exomiser.core.phenotype.PhenotypeTerm;
@@ -32,9 +33,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -44,6 +45,9 @@ public class TestPriorityServiceFactory {
 
     private static final Logger logger = LoggerFactory.getLogger(TestPriorityServiceFactory.class);
 
+    static {
+        logger.info("Creating test ontology and priority services - this is static test data");
+    }
     public static final OntologyService TEST_ONTOLOGY_SERVICE = setUpOntologyService();
 
     public static final PriorityService TEST_SERVICE = setUpPriorityService();
@@ -63,37 +67,18 @@ public class TestPriorityServiceFactory {
         return new PriorityService(stubModelService, new PhenotypeMatchService(stubOntologyService), null);
     }
 
-    private static PriorityService setUpPriorityService() {
-        logger.info("Creating test priority service - this is static test data");
-
-        OntologyService testOntologyService = setUpOntologyService();
-
-        logger.info("To the following models:");
-        ModelService testModelService = setUpModelService();
-
-        logger.info("Associated with genes:");
-        logger.info("    Entrez:2263 - FGFR2");
-        logger.info("    Entrez:4920 - ROR2");
-        logger.info("    Entrez:341640 - FREM2");
-
-        List<Disease> diseases = TestPrioritiserDataFileReader.readDiseaseData("src/test/resources/prioritisers/disease-models");
-        TestDiseaseDao testDiseaseDao = new TestDiseaseDao(diseases);
-
-        return new PriorityService(testModelService, new PhenotypeMatchService(testOntologyService), testDiseaseDao);
-    }
-
     private static OntologyService setUpOntologyService() {
-        Map<String, PhenotypeTerm> hpPhenotypesTerms = new HashMap<>();
-        hpPhenotypesTerms.put("HP:0010055", PhenotypeTerm.of("HP:0010055", "Broad hallux"));
-        hpPhenotypesTerms.put("HP:0001363", PhenotypeTerm.of("HP:0001363", "Craniosynostosis"));
-        hpPhenotypesTerms.put("HP:0001156", PhenotypeTerm.of("HP:0001156", "Brachydactyly syndrome"));
-        hpPhenotypesTerms.put("HP:0011304", PhenotypeTerm.of("HP:0011304", "Broad thumb"));
+        List<PhenotypeMatch> hpHpMappings = TestPrioritiserDataFileReader.readOntologyMatchData("src/test/resources/prioritisers/hp-hp-mappings");
+
+        Map<String, PhenotypeTerm> hpPhenotypesTerms = hpHpMappings.stream()
+                .map(PhenotypeMatch::getQueryPhenotype)
+                .distinct()
+                .collect(Collectors.toConcurrentMap(PhenotypeTerm::getId, Function.identity()));
 
         logger.info("This data links {} phenotypes:", hpPhenotypesTerms.size());
         hpPhenotypesTerms.values().forEach(term ->logger.info("    {} - {}", term.getId(), term.getLabel()));
 
         logger.info("Via cross-species phenotype mappings:");
-        List<PhenotypeMatch> hpHpMappings = TestPrioritiserDataFileReader.readOntologyMatchData("src/test/resources/prioritisers/hp-hp-mappings");
         logger.info("    hp-hp: " + hpHpMappings.size());
 
         List<PhenotypeMatch> hpMpMappings = TestPrioritiserDataFileReader.readOntologyMatchData("src/test/resources/prioritisers/hp-mp-mappings");
@@ -108,6 +93,26 @@ public class TestPriorityServiceFactory {
                 .setHumanMouseMappings(createPhenotypeMap(hpMpMappings))
                 .setHumanFishMappings(createPhenotypeMap(hpZpMappings))
                 .build();
+    }
+
+    private static PriorityService setUpPriorityService() {
+        logger.info("To the following models:");
+        ModelService testModelService = setUpModelService();
+
+        logger.info("Associated with genes:");
+        testModelService.getHumanGeneDiseaseModels().stream()
+                .map(geneModel -> GeneIdentifier.builder()
+                        .entrezId(geneModel.getEntrezGeneId().toString())
+                        .geneSymbol(geneModel.getHumanGeneSymbol())
+                        .build())
+                .distinct()
+                .forEach(geneIdentifier -> logger.info("    Entrez:{} - {}", geneIdentifier.getEntrezId(), geneIdentifier
+                        .getGeneSymbol()));
+
+        List<Disease> diseases = TestPrioritiserDataFileReader.readDiseaseData("src/test/resources/prioritisers/disease-models");
+        TestDiseaseDao testDiseaseDao = new TestDiseaseDao(diseases);
+
+        return new PriorityService(testModelService, new PhenotypeMatchService(TestPriorityServiceFactory.TEST_ONTOLOGY_SERVICE), testDiseaseDao);
     }
 
     private static Map<PhenotypeTerm, List<PhenotypeMatch>> createPhenotypeMap(List<PhenotypeMatch> crossOntologyMappings) {
