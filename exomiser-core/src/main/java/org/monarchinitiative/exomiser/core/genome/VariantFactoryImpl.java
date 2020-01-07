a7b9baea7a3a347c5e5597706435a93c8a83bc2b
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-01-07 13:18:28 +0000

    Add static modifier to VariantFactoryImpl.VariantCounter

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/genome/VariantFactoryImpl.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/genome/VariantFactoryImpl.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2019 Queen Mary University of London.
+ * Copyright (c) 2016-2020 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -154,7 +154,7 @@ public class VariantFactoryImpl implements VariantFactory {
     /**
      * Data class for tracking number of annotated variants
      */
-    private class VariantCounter {
+    private static class VariantCounter {
         final AtomicInteger variantRecords = new AtomicInteger(0);
         final AtomicInteger structuralVariants = new AtomicInteger(0);
         final AtomicInteger unannotatedVariants = new AtomicInteger(0);
