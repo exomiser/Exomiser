464c7fe4c052a7436e8a6d75c041ddc2ac0eeb6d
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-03-31 17:05:15 +0100

    Add new OutputSettingsProtoConverter

---
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/writers/OutputSettingsProtoConverter.java
@@ -0,0 +1,61 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.monarchinitiative.exomiser.core.writers;
+
+import com.google.common.collect.Sets;
+import org.monarchinitiative.exomiser.api.v1.OutputProto;
+import org.monarchinitiative.exomiser.core.proto.ProtoConverter;
+
+/**
+ * Class for inter-converting output settings/options between the domain and proto formats.
+ *
+ * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
+ * @since 13.0.0
+ */
+public class OutputSettingsProtoConverter implements ProtoConverter<OutputSettings, OutputProto.OutputOptions> {
+
+    @Override
+    public OutputProto.OutputOptions toProto(OutputSettings outputOptions) {
+        return OutputProto.OutputOptions.newBuilder()
+                .setOutputPrefix(outputOptions.getOutputPrefix())
+                .setNumGenes(outputOptions.getNumberOfGenesToShow())
+                .setOutputContributingVariantsOnly(outputOptions.outputContributingVariantsOnly())
+                .addAllOutputFormats(outputOptions
+                        .getOutputFormats()
+                        .stream()
+                        .map(outputFormat -> OutputProto.OutputFormat.valueOf(outputFormat.toString()))
+                        .collect(Sets.toImmutableEnumSet()))
+                .build();
+    }
+
+    @Override
+    public OutputSettings toDomain(OutputProto.OutputOptions outputOptions) {
+        return OutputSettings.builder()
+                .outputPrefix(outputOptions.getOutputPrefix())
+                .numberOfGenesToShow(outputOptions.getNumGenes())
+                .outputContributingVariantsOnly(outputOptions.getOutputContributingVariantsOnly())
+                .outputFormats(outputOptions
+                        .getOutputFormatsList().stream()
+                        .map(format -> OutputFormat.parseFormat(format.toString()))
+                        .collect(Sets.toImmutableEnumSet()))
+                .build();
+    }
+}
---
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/writers/OutputSettingsProtoConverterTest.java
@@ -0,0 +1,62 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.monarchinitiative.exomiser.core.writers;
+
+import org.junit.jupiter.api.Test;
+import org.monarchinitiative.exomiser.api.v1.OutputProto;
+
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
+ */
+class OutputSettingsProtoConverterTest {
+
+    private final OutputSettingsProtoConverter instance = new OutputSettingsProtoConverter();
+
+    private final OutputProto.OutputOptions proto = OutputProto.OutputOptions.newBuilder()
+            .setOutputContributingVariantsOnly(true)
+            .setNumGenes(10)
+            .setOutputPrefix("frood")
+            .addOutputFormats(OutputProto.OutputFormat.HTML)
+            .addOutputFormats(OutputProto.OutputFormat.JSON)
+            .build();
+
+    private final OutputSettings domain = OutputSettings.builder()
+            .outputContributingVariantsOnly(true)
+            .numberOfGenesToShow(10)
+            .outputPrefix("frood")
+            .outputFormats(Set.of(OutputFormat.HTML, OutputFormat.JSON))
+            .build();
+
+    @Test
+    void toProto() {
+        assertThat(instance.toProto(domain), equalTo(proto));
+    }
+
+    @Test
+    void toDomain() {
+        assertThat(instance.toDomain(proto), equalTo(domain));
+    }
+}
