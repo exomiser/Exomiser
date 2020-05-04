8552cfdda0f6baa206d5ac2642022b869ed7669e
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-05-04 18:24:18 +0100

    Unbox early in JannovarAnnotationService

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/genome/JannovarAnnotationService.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/genome/JannovarAnnotationService.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2019 Queen Mary University of London.
+ * Copyright (c) 2016-2020 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -86,7 +86,7 @@ public class JannovarAnnotationService {
         return new GenomePosition(referenceDictionary, Strand.FWD, chr, pos, PositionType.ONE_BASED);
     }
 
-    private Integer getIntValueOfChromosomeOrZero(String contig) {
+    private int getIntValueOfChromosomeOrZero(String contig) {
         return referenceDictionary.getContigNameToID().getOrDefault(contig, UNKNOWN_CHROMOSOME);
     }
 
