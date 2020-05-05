dead25510d2b83075dcf036e7b2fde7324d65edf
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-05-05 19:29:34 +0100

    Update JannovarFactoryConfig to specify downloadDir when building genome data

--- exomiser-data-genome/src/main/java/org/monarchinitiative/exomiser/data/genome/config/JannovarFactoryConfig.java
+++ exomiser-data-genome/src/main/java/org/monarchinitiative/exomiser/data/genome/config/JannovarFactoryConfig.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2019 Queen Mary University of London.
+ * Copyright (c) 2016-2020 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -45,7 +45,8 @@ public class JannovarFactoryConfig {
     @Bean
     JannovarDataFactory jannovarDataFactory() {
         Path jannovarIniFilePath = getPathForProperty("jannovar.ini-file");
-        return JannovarDataFactory.builder(jannovarIniFilePath).build();
+        Path buildDir = getPathForProperty("build-dir").resolve("jannovar");
+        return JannovarDataFactory.builder(jannovarIniFilePath).downloadDir(buildDir).build();
     }
 
     private Path getPathForProperty(String propertyKey) {
