a277f35a4a99c8d7d04f8fe320e243da692b871a
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-05-26 10:39:47 +0100

    Add new OrphanetInheritanceXmlParser to data-phenotype

---
+++ exomiser-data-phenotype/src/main/java/org/monarchinitiative/exomiser/data/phenotype/parsers/OrphanetInheritanceXmlParser.java
@@ -0,0 +1,265 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.monarchinitiative.exomiser.data.phenotype.parsers;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import org.monarchinitiative.exomiser.core.prioritisers.model.InheritanceMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.monarchinitiative.exomiser.core.prioritisers.model.InheritanceMode.*;
+
+/**
+ * Parser for extracting Disease MOI and age of onset from Orphanet en_product9_ages.xml file. This was shamelessly
+ * adapted from the Phenol class OrphanetInheritanceXMLParser
+ *
+ * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
+ */
+public class OrphanetInheritanceXmlParser {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrphanetInheritanceXmlParser.class);
+
+    /**
+     * Path to {@code en_product9_age.xml} file.
+     */
+    private final String orphanetXmlPath;
+    /**
+     * Key: an Orphanet disease id; value: an array list of HpoAnnotations, one for each inheritance mode
+     * that is associated with the disease.
+     */
+    private final Multimap<String, InheritanceMode> disease2inheritanceMultimap;
+    // XML Parsing
+    private static final String DISORDER = "Disorder";
+    private static final String ORPHA_NUMBER = "OrphaNumber";
+    private static final String NAME = "Name";
+    private static final String DISORDER_TYPE = "DisorderType";
+    private static final String DISORDER_GROUP = "DisorderGroup";
+    private static final String TYPE_OF_INHERITANCE_LIST = "TypeOfInheritanceList";
+    private static final String TYPE_OF_INHERITANCE = "TypeOfInheritance";
+    private static final String AVERAGE_AGE_OF_ONSET_LIST = "AverageAgeOfOnsetList";
+    private static final String AVERAGE_AGE_OF_DEATH_LIST = "AverageAgeOfDeathList";
+    /**
+     * Orphanet marks some of its intheritance entries as Not applicable. We will just skip them.
+     * This is the corresponding ID.
+     */
+    // TODO: WARNING! These ids might change from release to release and therefore might be the wrong things to be using.
+    private static final String NOT_APPLICABLE_ID = "23494"; // Not applicable
+    /**
+     * similar to above. We will skip this.
+     */
+    private static final String UNKNOWN_ID = "23480"; // Unknown
+    /**
+     * similar to above. We will skip this.
+     */
+    private static final String NO_DATA_AVAILABLE = "23487"; // No data available
+
+    private boolean inDisorder = false;
+    private boolean inDisorderType = false;
+    private boolean inDisorderGroup = false;
+    private boolean isInAverageAgeOfDeathList = false;
+    private boolean inAverageAgeOfOnsetList = false;
+    private boolean inTypeOfInheritanceList = false;
+    private boolean inTypeOfInheritance = false;
+
+    public OrphanetInheritanceXmlParser(String xmlpath) {
+        orphanetXmlPath = xmlpath;
+        disease2inheritanceMultimap = ArrayListMultimap.create();
+        parse(new File(orphanetXmlPath));
+    }
+
+    public Multimap<String, InheritanceMode> getDisease2inheritanceMultimap() {
+        return disease2inheritanceMultimap;
+    }
+
+    private void parse(File file) {
+
+//    <Disorder id = "17601" >
+//      <OrphaNumber > 166024 </OrphaNumber >
+//      <ExpertLink lang = "en" > http://www.orpha.net/consor/cgi-bin/OC_Exp.php?lng=en&amp;Expert=166024</ExpertLink>
+//      <Name lang = "en" > Multiple epiphyseal dysplasia, Al -Gazali type</Name >
+//      <DisorderType id = "21394" >
+//        <Name lang = "en" > Disease </Name >
+//      </DisorderType >
+//      <DisorderGroup id = "36547" >
+//        <Name lang = "en" > Disorder </Name >
+//      </DisorderGroup >
+//      <AverageAgeOfOnsetList count = "2" >
+//        <AverageAgeOfOnset id = "23522" >
+//          <Name lang = "en" > Infancy </Name >
+//        </AverageAgeOfOnset >
+//        <AverageAgeOfOnset id = "23515" >
+//          <Name lang = "en" > Neonatal </Name >
+//        </AverageAgeOfOnset >
+//      </AverageAgeOfOnsetList >
+//      <AverageAgeOfDeathList count = "0" >
+//      </AverageAgeOfDeathList >
+//      <TypeOfInheritanceList count = "1" >
+//        <TypeOfInheritance id = "23417" >
+//          <Name lang = "en" > Autosomal recessive</Name >
+//        </TypeOfInheritance >
+//      </TypeOfInheritanceList >
+//    </Disorder >
+
+        try (InputStream in = new FileInputStream(file)) {
+            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+            String currentOrphanum = null;
+            String currentDiseaseName = null;
+            String currentInheritanceId = null;
+            String currentModeOfInheritanceLabel = null;
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+                if (event.isStartElement()) {
+                    StartElement startElement = event.asStartElement();
+                    final String localPart = startElement.getName().getLocalPart();
+                    if (localPart.equals(DISORDER)) {
+                        inDisorder = true;
+                    } else if (inDisorder &&
+                            !inDisorderType &&
+                            !inDisorderGroup &&
+                            !inAverageAgeOfOnsetList &&
+                            !isInAverageAgeOfDeathList &&
+                            !inTypeOfInheritance &&
+                            localPart.equals(ORPHA_NUMBER)) {
+                        event = eventReader.nextEvent(); // go to the contents of the node
+                        currentOrphanum = event.asCharacters().getData();
+                    } else if (inDisorder &&
+                            !inDisorderType &&
+                            !inDisorderGroup &&
+                            !inAverageAgeOfOnsetList &&
+                            !isInAverageAgeOfDeathList &&
+                            !inTypeOfInheritance &&
+                            localPart.equals(NAME)) {
+                        event = eventReader.nextEvent();
+                        currentDiseaseName = event.asCharacters().getData();
+                    } else if (inDisorder && localPart.equals(TYPE_OF_INHERITANCE_LIST)) {
+                        inTypeOfInheritanceList = true;
+                        Attribute countAttribute = startElement.getAttributeByName(QName.valueOf("count"));
+                        if (countAttribute == null || "0".equals(countAttribute.getValue())) {
+                            // there are no inheritance modes for this disease - add
+                            logger.info("Skipping ORPHA:{} {} due to no known MOI", currentOrphanum, currentDiseaseName);
+                        }
+                    } else if (inTypeOfInheritanceList && localPart.equals(TYPE_OF_INHERITANCE)) {
+                        inTypeOfInheritance = true;
+                        // TODO: WARNING! These ids might change from release to release and therefore might be the wrong
+                        //  things to be using.
+                        Attribute idAttribute = startElement.getAttributeByName(QName.valueOf("id"));
+                        currentInheritanceId = idAttribute.getValue();
+                    } else if (inDisorder &&
+                            !inAverageAgeOfOnsetList &&
+                            !isInAverageAgeOfDeathList &&
+                            !inDisorderType &&
+                            inTypeOfInheritance &&
+                            localPart.equals(NAME)) {
+                        event = eventReader.nextEvent(); // go to the contents of the node
+                        currentModeOfInheritanceLabel = event.asCharacters().getData();
+                        if (currentInheritanceId == null || currentInheritanceId.equals(NOT_APPLICABLE_ID) ||
+                                currentInheritanceId.equals(UNKNOWN_ID) ||
+                                currentInheritanceId.equals(NO_DATA_AVAILABLE)) {
+                            continue;
+                        }
+                        InheritanceMode inheritanceMode = getHpoInheritanceTermId(currentInheritanceId, currentModeOfInheritanceLabel);
+                        if (inheritanceMode != null) {
+                            String disId = String.format("ORPHA:%s", currentOrphanum);
+                            logger.info("{} {} {} {}", disId, currentModeOfInheritanceLabel, inheritanceMode.getInheritanceCode(), currentDiseaseName);
+                            this.disease2inheritanceMultimap.put(disId, inheritanceMode);
+                        }
+                    } else if (localPart.equals(AVERAGE_AGE_OF_ONSET_LIST)) {
+                        inAverageAgeOfOnsetList = true;
+                    } else if (localPart.equals(AVERAGE_AGE_OF_DEATH_LIST)) {
+                        isInAverageAgeOfDeathList = true;
+                    } else if (localPart.equals(TYPE_OF_INHERITANCE)) {
+                        inTypeOfInheritance = true;
+                    } else if (localPart.equals(DISORDER_TYPE)) {
+                        inDisorderType = true;
+                    } else if (localPart.equals(DISORDER_GROUP)) {
+                        inDisorderGroup = true;
+                    }
+                } else if (event.isEndElement()) {
+                    EndElement endElement = event.asEndElement();
+                    String localPart = endElement.getName().getLocalPart();
+                    if (localPart.equals(DISORDER)) {
+                        inDisorder = false;
+                    } else if (localPart.equals(TYPE_OF_INHERITANCE_LIST)) {
+                        inTypeOfInheritanceList = false;
+                    } else if (localPart.equals(TYPE_OF_INHERITANCE)) {
+                        inTypeOfInheritance = false;
+                    } else if (localPart.equals(AVERAGE_AGE_OF_ONSET_LIST)) {
+                        inAverageAgeOfOnsetList = false;
+                    } else if (localPart.equals(AVERAGE_AGE_OF_DEATH_LIST)) {
+                        isInAverageAgeOfDeathList = false;
+                    } else if (localPart.equals(DISORDER_TYPE)) {
+                        inDisorderType = false;
+                    } else if (localPart.equals(DISORDER_GROUP)) {
+                        inDisorderGroup = false;
+                    }
+                }
+            }
+        } catch (IOException | XMLStreamException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private InheritanceMode getHpoInheritanceTermId(String orphaInheritanceId, String orphaLabel) {
+        if (orphaInheritanceId.equals("23417") && orphaLabel.equals("Autosomal recessive")) {
+            return AUTOSOMAL_RECESSIVE;
+        } else if (orphaInheritanceId.equals("23410") && orphaLabel.equals("Autosomal dominant")) {
+            return AUTOSOMAL_DOMINANT;
+        } else if (orphaInheritanceId.equals("23424") && orphaLabel.equals("Multigenic/multifactorial")) {
+            return POLYGENIC; //TODO MULTIFACTORIAL - currently InheritanceMode only contains monogenic or polygenic MOIs
+        } else if (orphaInheritanceId.equals("23431") && orphaLabel.equals("X-linked recessive")) {
+            return X_RECESSIVE;
+        } else if (orphaInheritanceId.equals("23438") && orphaLabel.equals("Mitochondrial inheritance")) {
+            return MITOCHONDRIAL;
+        } else if (orphaInheritanceId.equals("23445") && orphaLabel.equals("X-linked dominant")) {
+            return X_DOMINANT;
+        } else if (orphaInheritanceId.equals("23473") && orphaLabel.equals("Y-linked")) {
+            return Y_LINKED;
+        } else if (orphaInheritanceId.equals("23466") && orphaLabel.equals("Semi-dominant")) {
+            return UNKNOWN; //TODO SEMIDOMINANT
+        } else if (orphaInheritanceId.equals("409936") && orphaLabel.equals("Oligogenic")) {
+            return POLYGENIC; //TODO OLIGOGENIC
+        } else if (orphaInheritanceId.equals("23480") && orphaLabel.equals("Unknown")) {
+            return UNKNOWN;
+        } else if (orphaInheritanceId.equals("23487") && orphaLabel.equals("No data available")) {
+            return UNKNOWN;
+        } else if (orphaInheritanceId.equals("23494") && orphaLabel.equals("Not applicable")) {
+            return null;
+        } else {
+            logger.warn("Could not find HPO id for Orphanet inheritence entry: {} ({})", orphaLabel, orphaInheritanceId);
+            return null;
+        }
+    }
+}
---
+++ exomiser-data-phenotype/src/test/java/org/monarchinitiative/exomiser/data/phenotype/parsers/OrphanetInheritanceXmlParserTest.java
@@ -0,0 +1,64 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.monarchinitiative.exomiser.data.phenotype.parsers;
+
+import com.google.common.collect.Multimap;
+import org.junit.jupiter.api.Test;
+import org.monarchinitiative.exomiser.core.prioritisers.model.InheritanceMode;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.monarchinitiative.exomiser.core.prioritisers.model.InheritanceMode.*;
+
+/**
+ * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
+ */
+class OrphanetInheritanceXmlParserTest {
+
+    @Test
+    void getDisease2inheritanceMultimap() {
+        OrphanetInheritanceXmlParser instance = new OrphanetInheritanceXmlParser("src/test/resources/data/en_product9_ages.xml");
+
+        Multimap<String, InheritanceMode> disease2inheritanceMultimap = instance.getDisease2inheritanceMultimap();
+        assertThat(disease2inheritanceMultimap.get("ORPHA:166024"), equalTo(List.of(AUTOSOMAL_RECESSIVE)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:58"), equalTo(List.of(AUTOSOMAL_DOMINANT)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:166084"), equalTo(List.of(AUTOSOMAL_DOMINANT, AUTOSOMAL_RECESSIVE)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:535"), equalTo(List.of(POLYGENIC)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:166119"), equalTo(List.of(AUTOSOMAL_DOMINANT)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:487"), equalTo(List.of(AUTOSOMAL_RECESSIVE)));
+
+        assertThat(disease2inheritanceMultimap.get("ORPHA:461"), equalTo(List.of(X_RECESSIVE)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:586"), equalTo(List.of(AUTOSOMAL_RECESSIVE)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:261"), equalTo(List.of(AUTOSOMAL_DOMINANT, AUTOSOMAL_RECESSIVE, X_RECESSIVE)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:550"), equalTo(List.of(MITOCHONDRIAL)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:480"), equalTo(List.of(AUTOSOMAL_RECESSIVE, MITOCHONDRIAL)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:163937"), equalTo(List.of(X_DOMINANT)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:163966"), equalTo(List.of(X_DOMINANT)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:1941"), equalTo(List.of(POLYGENIC)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:99771"), equalTo(List.of(POLYGENIC)));
+        assertThat(disease2inheritanceMultimap.get("ORPHA:99792"), equalTo(List.of()));
+        assertFalse(disease2inheritanceMultimap.containsKey("ORPHA:99792"));
+
+    }
+}
