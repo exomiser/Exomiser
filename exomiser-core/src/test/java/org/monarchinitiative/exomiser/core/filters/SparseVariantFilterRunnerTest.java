a9c9758f3caac5a592c86effa88d83a72a23b21d
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-09-21 16:45:04 +0100

    Make SparseVariantFilterRunner instance final in SparseVariantFilterRunnerTest

--- exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/filters/SparseVariantFilterRunnerTest.java
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/filters/SparseVariantFilterRunnerTest.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2018 Queen Mary University of London.
+ * Copyright (c) 2016-2020 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -49,7 +49,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class SparseVariantFilterRunnerTest {
 
-    private SparseVariantFilterRunner instance = new SparseVariantFilterRunner();
+    private final SparseVariantFilterRunner instance = new SparseVariantFilterRunner();
 
     @Mock
     private VariantEffectFilter variantEffectFilter;
