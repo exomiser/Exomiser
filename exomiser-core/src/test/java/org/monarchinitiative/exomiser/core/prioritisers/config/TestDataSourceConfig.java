d933e1beba686bf20b5c3ea22509044ae5902e83
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-09-21 16:43:15 +0100

    Update TestDataSourceConfig to remove DATABASE_TO_LOWER=TRUE in phenotypeDataSource URL

--- exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/prioritisers/config/TestDataSourceConfig.java
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/prioritisers/config/TestDataSourceConfig.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2019 Queen Mary University of London.
+ * Copyright (c) 2016-2020 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -39,7 +39,7 @@ public class TestDataSourceConfig {
 
     @Bean("phenotypeDataSource")
     public DataSource dataSource() {
-        String url = "jdbc:h2:mem:exomiser;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;";
+        String url = "jdbc:h2:mem:exomiser;MODE=PostgreSQL;";
         String user = "sa";
         String password = "";
 
