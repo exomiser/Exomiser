85299b16a879c1ebe1f987997b052f90f7f224da
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-12-09 11:45:58 +0000

    Remove public modifiers from DataMatrix methods

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/util/DataMatrix.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/util/DataMatrix.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2018 Queen Mary University of London.
+ * Copyright (c) 2016-2020 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -34,10 +34,11 @@ public interface DataMatrix {
 
     /**
      * Returns a empty, immutable {@code DataMatrix}
+     *
      * @return an immutable, empty {@code DataMatrix}
      * @since 10.0.0
      */
-    public static DataMatrix empty() {
+    static DataMatrix empty() {
         return StubDataMatrix.empty();
     }
 
@@ -46,33 +47,31 @@ public interface DataMatrix {
      *
      * @return a map representation of the entrez gene id of a gene and its row position in the {@code DataMatrix}
      */
-    public Map<Integer, Integer> getEntrezIdToRowIndex();
+    Map<Integer, Integer> getEntrezIdToRowIndex();
 
     /**
      * The {@code FloatMatrix} representation of the data, without an index.
      *
      * @return a {@code FloatMatrix} representation of the data, without an index.
      */
-    public FloatMatrix getMatrix();
+    FloatMatrix getMatrix();
 
     /**
-     *
      * @return the number of rows in this {@code DataMatrix}
      */
-    public int numRows();
+    int numRows();
 
     /**
-     *
      * @return the number of columns in this {@code DataMatrix}
      */
-    public int numColumns();
+    int numColumns();
 
     /**
      *
      * @param entrezGeneId the entrez gene identifier of the gene
      * @return true if the argument {@code entrezGeneId} is contained in the matrix
      */
-    public boolean containsGene(Integer entrezGeneId);
+    boolean containsGene(Integer entrezGeneId);
 
     /**
      * Finds the {@code Integer} row index for the argument gene identifier.
@@ -80,7 +79,7 @@ public interface DataMatrix {
      * @param entrezGeneId the entrez gene identifier of the gene
      * @return the {@code Integer} row index for this gene identifier or {@code null} if not present.
      */
-    public Integer getRowIndexForGene(int entrezGeneId);
+    Integer getRowIndexForGene(int entrezGeneId);
 
     /**
      * Finds the {@code FloatMatrix} column for the argument gene identifier.
@@ -88,6 +87,6 @@ public interface DataMatrix {
      * @param entrezGeneId the entrez gene identifier of the gene
      * @return the {@code FloatMatrix} column for this gene identifier or {@code null} if not present.
      */
-    public FloatMatrix getColumnMatrixForGene(int entrezGeneId);
+    FloatMatrix getColumnMatrixForGene(int entrezGeneId);
 
 }
