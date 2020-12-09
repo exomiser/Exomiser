35ce560b467c7035c91b074a0493e17010c2ef68
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-12-09 13:27:37 +0000

    Switch VariantAllele field order

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/VariantAllele.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/VariantAllele.java
@@ -33,8 +33,8 @@ public class VariantAllele implements VariantCoordinates {
     private final int start;
     private final ConfidenceInterval startCi;
 
-    private final int end;
     private final String endContig;
+    private final int end;
     private final ConfidenceInterval endCi;
 
     private final int length;
