157f108a01d69a3dfb21940727bd8850790c53d8
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-03-24 11:28:06 +0000

    Remove Junit 4 assertThat from exomiser-web SelectOptionTest

--- exomiser-web/src/test/java/org/monarchinitiative/exomiser/web/model/SelectOptionTest.java
+++ exomiser-web/src/test/java/org/monarchinitiative/exomiser/web/model/SelectOptionTest.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2018 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -28,8 +28,8 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.lessThan;
-import static org.junit.Assert.assertThat;
 
 /**
  * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
