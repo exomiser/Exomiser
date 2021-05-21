ed386384847921d568e90629e381ca9cace81cc9
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-05-21 16:03:08 +0100

    Add insert statements for SV data

---
+++ exomiser-data-genome/src/main/resources/db/migration/V1.3.2__insert_dgv_sv.sql
@@ -0,0 +1,25 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2021 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+INSERT INTO sv_freq
+SELECT *
+FROM CSVREAD('${import.path}/dgv-sv.pg',
+             'chromosome|start|end|change_length|variant_type|db_var_id|source|source_id|allele_count|allele_number',
+             'charset=UTF-8 fieldDelimiter='' fieldSeparator=| nullString=NULL');
