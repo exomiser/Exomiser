388dd35ac52acd7a1dfd79c07d5087274f8c3b04
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-05-21 16:02:05 +0100

    Update data-genome schema to allow for longer gnomAD-SV identifiers

--- exomiser-data-genome/src/main/resources/db/migration/V1.0.0__create_schema.sql
+++ exomiser-data-genome/src/main/resources/db/migration/V1.0.0__create_schema.sql
@@ -18,6 +18,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2021 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 DROP TABLE IF EXISTS regulatory_regions;
 
 CREATE TABLE regulatory_regions
@@ -59,7 +79,7 @@ CREATE TABLE sv_freq
     variant_type  CHARACTER VARYING(20),
     db_var_id     CHARACTER VARYING(20),
     source        CHARACTER VARYING(20),
-    source_id     CHARACTER VARYING(20),
+    source_id     CHARACTER VARYING(40),
     allele_count  INTEGER  NOT NULL,
     allele_number INTEGER  NOT NULL
 );
