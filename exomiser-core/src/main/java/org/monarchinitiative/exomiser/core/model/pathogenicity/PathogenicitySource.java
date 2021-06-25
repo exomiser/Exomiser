7956654b25ed3afcddde699cc995f86f85238229
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-06-25 16:52:00 +0100

    Update PathogenicitySource.ISCA to CLINVAR

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/pathogenicity/PathogenicitySource.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/model/pathogenicity/PathogenicitySource.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -57,5 +57,5 @@ public enum PathogenicitySource {
 
     // SV data sources
     DBVAR,
-    ISCA
+    CLINVAR
 }
