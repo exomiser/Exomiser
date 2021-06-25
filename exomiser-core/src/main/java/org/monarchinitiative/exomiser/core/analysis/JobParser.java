0fff416f11cc9ef5f98a9ec9a05013722cf25bb3
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-06-25 16:51:00 +0100

    Update JobParser to use AnalysisPresetBuilder when applying presets

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/JobParser.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/JobParser.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -48,7 +48,7 @@ import java.util.*;
 import java.util.Map.Entry;
 
 import static java.util.stream.Collectors.toList;
-import static org.monarchinitiative.exomiser.core.model.pathogenicity.PathogenicitySource.*;
+import static org.monarchinitiative.exomiser.core.model.pathogenicity.PathogenicitySource.valueOf;
 
 /**
  * Class for parsing {@link Sample} and {@link Analysis} domain objects out of the protobuf {@link JobProto} objects.
@@ -66,53 +66,19 @@ import static org.monarchinitiative.exomiser.core.model.pathogenicity.Pathogenic
 @Component
 public class JobParser {
 
-    private static final InheritanceModeOptions DEFAULT_INHERITANCE_MODE_OPTIONS;
-
-    static {
-        Map<SubModeOfInheritance, Float> inheritanceModeFrequencyCutoffs = new EnumMap<>(SubModeOfInheritance.class);
-        // all frequencies are in percentage values
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.AUTOSOMAL_DOMINANT, 0.1f);
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.AUTOSOMAL_RECESSIVE_COMP_HET, 2.0f);
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.AUTOSOMAL_RECESSIVE_HOM_ALT, 0.1f);
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.X_DOMINANT, 0.1f);
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.X_RECESSIVE_COMP_HET, 2.0f);
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.X_RECESSIVE_HOM_ALT, 0.1f);
-        inheritanceModeFrequencyCutoffs.put(SubModeOfInheritance.MITOCHONDRIAL, 0.2f);
-        DEFAULT_INHERITANCE_MODE_OPTIONS = InheritanceModeOptions.of(inheritanceModeFrequencyCutoffs);
-    }
-
-    private static final EnumSet<FrequencySource> DEFAULT_FREQUENCY_SOURCES = EnumSet.of(
-            FrequencySource.ESP_AFRICAN_AMERICAN, FrequencySource.ESP_ALL, FrequencySource.ESP_EUROPEAN_AMERICAN,
-            FrequencySource.THOUSAND_GENOMES,
-            FrequencySource.EXAC_AFRICAN_INC_AFRICAN_AMERICAN, FrequencySource.EXAC_AMERICAN,
-            FrequencySource.EXAC_EAST_ASIAN, FrequencySource.EXAC_FINNISH,
-            FrequencySource.EXAC_NON_FINNISH_EUROPEAN, FrequencySource.EXAC_SOUTH_ASIAN, FrequencySource.EXAC_OTHER,
-            FrequencySource.UK10K, FrequencySource.TOPMED,
-            FrequencySource.GNOMAD_E_AFR, FrequencySource.GNOMAD_E_AMR,
-            //FrequencySource.GNOMAD_E_ASJ,
-            FrequencySource.GNOMAD_E_EAS, FrequencySource.GNOMAD_E_FIN,
-            FrequencySource.GNOMAD_E_NFE, FrequencySource.GNOMAD_E_OTH, FrequencySource.GNOMAD_E_SAS,
-            FrequencySource.GNOMAD_G_AFR, FrequencySource.GNOMAD_G_AMR,
-            //FrequencySource.GNOMAD_G_ASJ,
-            FrequencySource.GNOMAD_G_EAS, FrequencySource.GNOMAD_G_FIN,
-            FrequencySource.GNOMAD_G_NFE, FrequencySource.GNOMAD_G_OTH, FrequencySource.GNOMAD_G_SAS
-    );
-
-    private static final HiPhiveOptions HI_PHIVE_OPTIONS = HiPhiveOptions.builder()
-            .runParams("human,mouse,fish,ppi")
-            .build();
-
     private static final Logger logger = LoggerFactory.getLogger(JobParser.class);
 
     private final GenomeAnalysisServiceProvider genomeAnalysisServiceProvider;
     private final PriorityFactory prioritiserFactory;
     private final OntologyService ontologyService;
+    private final AnalysisPresetBuilder analysisPresetBuilder;
 
     @Autowired
     public JobParser(GenomeAnalysisServiceProvider genomeAnalysisServiceProvider, PriorityFactory prioritiserFactory, OntologyService ontologyService) {
         this.genomeAnalysisServiceProvider = genomeAnalysisServiceProvider;
         this.prioritiserFactory = prioritiserFactory;
         this.ontologyService = ontologyService;
+        this.analysisPresetBuilder = new AnalysisPresetBuilder(genomeAnalysisServiceProvider, prioritiserFactory, ontologyService);
     }
 
     public Sample parseSample(JobProto.Job protoJob) {
@@ -133,11 +99,11 @@ public class JobParser {
     }
 
     public Analysis exomePreset() {
-        return buildExomePreset();
+        return analysisPresetBuilder.buildExomePreset();
     }
 
     public Analysis genomePreset() {
-        return buildGenomePreset();
+        return analysisPresetBuilder.buildGenomePreset();
     }
 
     private Sample getSample(JobProto.Job protoJob) {
@@ -194,57 +160,12 @@ public class JobParser {
         switch (protoJob.getPreset()) {
             case GENOME:
                 logger.info("Running GENOME preset");
-                return buildGenomePreset();
+                return analysisPresetBuilder.buildGenomePreset();
             case EXOME:
             default:
                 logger.info("Running EXOME preset");
-                return buildExomePreset();
+                return analysisPresetBuilder.buildExomePreset();
         }
-    }
-
-    private Analysis buildGenomePreset() {
-        return new AnalysisBuilder(genomeAnalysisServiceProvider, prioritiserFactory, ontologyService)
-                .analysisMode(AnalysisMode.PASS_ONLY)
-                .inheritanceModes(DEFAULT_INHERITANCE_MODE_OPTIONS)
-                .frequencySources(DEFAULT_FREQUENCY_SOURCES)
-                .pathogenicitySources(EnumSet.of(REVEL, MVP, REMM))
-                .addHiPhivePrioritiser(HI_PHIVE_OPTIONS)
-                .addPriorityScoreFilter(PriorityType.HIPHIVE_PRIORITY, 0.5f)// will remove a lot of the weak PPI hits
-                .addFailedVariantFilter()
-                .addRegulatoryFeatureFilter()
-                .addFrequencyFilter()
-                .addPathogenicityFilter(true)
-                .addInheritanceFilter()
-                .addOmimPrioritiser()
-                .build();
-    }
-
-    private Analysis buildExomePreset() {
-        return new AnalysisBuilder(genomeAnalysisServiceProvider, prioritiserFactory, ontologyService)
-                .analysisMode(AnalysisMode.PASS_ONLY)
-                .inheritanceModes(DEFAULT_INHERITANCE_MODE_OPTIONS)
-                .frequencySources(DEFAULT_FREQUENCY_SOURCES)
-                .pathogenicitySources(EnumSet.of(REVEL, MVP))
-                .addVariantEffectFilter(EnumSet.of(
-                        VariantEffect.FIVE_PRIME_UTR_EXON_VARIANT,
-                        VariantEffect.FIVE_PRIME_UTR_INTRON_VARIANT,
-                        VariantEffect.THREE_PRIME_UTR_EXON_VARIANT,
-                        VariantEffect.THREE_PRIME_UTR_INTRON_VARIANT,
-                        VariantEffect.NON_CODING_TRANSCRIPT_EXON_VARIANT,
-                        VariantEffect.NON_CODING_TRANSCRIPT_INTRON_VARIANT,
-                        VariantEffect.CODING_TRANSCRIPT_INTRON_VARIANT,
-                        VariantEffect.UPSTREAM_GENE_VARIANT,
-                        VariantEffect.DOWNSTREAM_GENE_VARIANT,
-                        VariantEffect.INTERGENIC_VARIANT,
-                        VariantEffect.REGULATORY_REGION_VARIANT
-                ))
-                .addFailedVariantFilter()
-                .addFrequencyFilter()
-                .addPathogenicityFilter(true)
-                .addInheritanceFilter()
-                .addOmimPrioritiser()
-                .addHiPhivePrioritiser(HI_PHIVE_OPTIONS)
-                .build();
     }
 
     private Analysis parseProtoAnalysis(AnalysisProto.Analysis protoAnalysis) {
--- exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/analysis/JobParserTest.java
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/analysis/JobParserTest.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -54,6 +54,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
  */
 class JobParserTest {
+
     private final GenomeAnalysisServiceProvider genomeAnalysisServiceProvider = new GenomeAnalysisServiceProvider(TestFactory
             .buildDefaultHg19GenomeAnalysisService());
     private final PriorityService priorityService = TestPriorityServiceFactory.stubPriorityService();
