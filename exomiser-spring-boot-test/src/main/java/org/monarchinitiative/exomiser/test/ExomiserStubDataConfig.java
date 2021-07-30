8c9539b781df84ba8ecaf670e4d47489c129d6d0
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-07-30 16:33:28 +0100

    Fix ExomiserStubDataConfig.genomeAnalysisService

--- exomiser-spring-boot-test/src/main/java/org/monarchinitiative/exomiser/test/ExomiserStubDataConfig.java
+++ exomiser-spring-boot-test/src/main/java/org/monarchinitiative/exomiser/test/ExomiserStubDataConfig.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2019 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -53,8 +53,7 @@ public class ExomiserStubDataConfig {
 
     @Bean
     public GenomeAnalysisService genomeAnalysisService() {
-        return new GenomeAnalysisServiceImpl(GenomeAssembly.HG19, Mockito.mock(GenomeAnalysisService.class), Mockito.mock(VariantDataService.class), Mockito
-                .mock(VariantFactory.class));
+        return new GenomeAnalysisServiceImpl(GenomeAssembly.HG19, Mockito.mock(GenomeAnalysisService.class), Mockito.mock(VariantDataService.class), Mockito.mock(VariantAnnotator.class));
     }
 
     @Bean
