7567f1a8a98c2a5c05dc02fc68a2cad95b66209e
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-09-13 13:47:22 +0100

    Update VariantDatabaseBuildRunner and MvStoreAlleleIndexer to use zero versions and compactMoveChunks before closing

--- exomiser-data-genome/src/main/java/org/monarchinitiative/exomiser/data/genome/VariantDatabaseBuildRunner.java
+++ exomiser-data-genome/src/main/java/org/monarchinitiative/exomiser/data/genome/VariantDatabaseBuildRunner.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -35,7 +35,6 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Main class for handling parsing of the {@link AlleleResource} and reading these into the variants.mv.db database.
@@ -57,48 +56,26 @@ public class VariantDatabaseBuildRunner {
     }
 
     public void run() {
-        MVStore mergeStore = new MVStore.Builder()
-                .fileName(buildPath.resolve(buildInfo.getBuildString() + "_variants_temp.mv.db").toString())
-                .compress()
-                .open();
-
-        // This is threadsafe and can be run in parallel. However, the throughput is significantly slower,
-        // to the extent that the overall time is the same, at least on my machine (4 cores) it is.
-        // This holds true both using parallelStream and a fixed thread pool executor with only 2 threads.
-        Indexer<Allele> alleleIndexer = new MvStoreAlleleIndexer(mergeStore);
-        alleleResources.forEach(alleleIndexer::index);
-
-        MVStore finalStore = new MVStore.Builder()
+        MVStore mvStore = new MVStore.Builder()
                 .fileName(buildPath.resolve(buildInfo.getBuildString() + "_variants.mv.db").toString())
                 .compress()
                 .open();
+        // this is key to keep the size of the store down when building otherwise it gets enormous
+        mvStore.setVersionsToKeep(0);
+        // This is threadsafe and can be run in parallel. However, the throughput is significantly slower,
+        // to the extent that the overall time is the same, at least on my machine (4 cores) it is.
+        // This holds true both using parallelStream and a fixed thread pool executor with only 2 threads.
+        Indexer<Allele> alleleIndexer = new MvStoreAlleleIndexer(mvStore);
+        alleleResources.forEach(alleleIndexer::index);
 
-        // MVStore stands for Multi-Version Store. Alleles appearing in multiple datasets will have a new version stored
-        // for each new dataset added.
-        // These multiple versions can make the initial store much larger than we need as we only want the final version
-        // of the allele. So as a workaround we're copying the entries from the original store to a new store which
-        // will only contain one version of each allele. This leads to significant space savings on disk - e.g. 25 GB original
-        // is only 12 GB when the final version is copied over. This operation takes about 40 min for 0.5 billion alleles.
-        copyToNewInstance(mergeStore, finalStore);
+        MVMap<AlleleKey, AlleleProperties> alleleMVMap = MvStoreUtil.openAlleleMVMap(mvStore);
+        logger.info("Written {} alleles to store", alleleMVMap.size());
 
-        mergeStore.close();
-        finalStore.close();
-    }
+        // super-important step for producing as small a store as possible, Could double (or more?) when this is in progress
+        logger.info("Compacting store...");
+        mvStore.compactMoveChunks();
 
-    private void copyToNewInstance(MVStore mvStore, MVStore newStore) {
-        MVMap<AlleleKey, AlleleProperties> map = MvStoreUtil.openAlleleMVMap(mvStore);
-
-        MVMap<AlleleKey, AlleleProperties> newMap = MvStoreUtil.openAlleleMVMap(newStore);
-
-        logger.info("Copying {} entries from temp store {} to final store {}", map.size(), mvStore.getFileStore().getFileName(), newStore.getFileStore().getFileName());
-        int count = 0;
-        for (Map.Entry<AlleleKey, AlleleProperties> entry : map.entrySet()) {
-            newMap.put(entry.getKey(), entry.getValue());
-            count++;
-            if (count % 10000000 == 0) {
-                logger.info("Written {} alleles", count);
-            }
-        }
-        logger.info("Finished copying {} entries to new map", newMap.size());
+        logger.info("Closing store");
+        mvStore.close();
     }
 }
--- exomiser-data-genome/src/main/java/org/monarchinitiative/exomiser/data/genome/indexers/MvStoreAlleleIndexer.java
+++ exomiser-data-genome/src/main/java/org/monarchinitiative/exomiser/data/genome/indexers/MvStoreAlleleIndexer.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -39,6 +39,7 @@ public class MvStoreAlleleIndexer extends AbstractIndexer<Allele> {
 
     public MvStoreAlleleIndexer(MVStore mvStore) {
         this.mvStore = mvStore;
+        this.mvStore.setVersionsToKeep(0);
         this.map = MvStoreUtil.openAlleleMVMap(mvStore);
     }
 
