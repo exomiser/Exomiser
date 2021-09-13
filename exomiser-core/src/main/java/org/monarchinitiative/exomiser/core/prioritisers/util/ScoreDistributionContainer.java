d09cd3225e655ac18d97329845ea3f9c785c1651
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-09-13 14:37:41 +0100

    Update ScoreDistributionContainer to use new folder layout with .out files in the out/ sub-directory of the distributionsFolder

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/util/ScoreDistributionContainer.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/util/ScoreDistributionContainer.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -109,7 +109,7 @@ public class ScoreDistributionContainer {
 	private synchronized void parseDistributions(int numQueryTerms) {
 
 		String key = getKey(symmetric, numQueryTerms);
-		String file = distributionsFolder + key + ".out";
+		String file = distributionsFolder + "out/" + key + ".out";
 
 		if (verboseParsing)
 			logger.info("Reading distributions from file: {}", file);
