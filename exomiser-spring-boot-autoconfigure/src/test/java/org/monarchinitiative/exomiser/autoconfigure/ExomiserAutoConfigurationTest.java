95231b2552677d0f3d7ec2773c2327da24f6c950
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-09-21 20:08:39 +0100

    Disable constantly failing ExomiserAutoConfigurationTest

--- exomiser-spring-boot-autoconfigure/src/test/java/org/monarchinitiative/exomiser/autoconfigure/ExomiserAutoConfigurationTest.java
+++ exomiser-spring-boot-autoconfigure/src/test/java/org/monarchinitiative/exomiser/autoconfigure/ExomiserAutoConfigurationTest.java
@@ -1,7 +1,7 @@
 /*
  * The Exomiser - A tool to annotate and prioritize genomic variants
  *
- * Copyright (c) 2016-2020 Queen Mary University of London.
+ * Copyright (c) 2016-2021 Queen Mary University of London.
  * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,6 +20,7 @@
 
 package org.monarchinitiative.exomiser.autoconfigure;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.monarchinitiative.exomiser.core.Exomiser;
 import org.monarchinitiative.exomiser.core.genome.GenomeAnalysisServiceProvider;
@@ -42,6 +43,7 @@ public class ExomiserAutoConfigurationTest extends AbstractAutoConfigurationTest
 
     // n.b there are issues with the @PreDestroy shutdown hooks for classes in the GenomeAnalysisServiceConfigurer using the MVStore
     // so all the relevant beans are being tested in one go
+    @Disabled("File lock exception when run as part of full test suite")
     @Test
     public void testAutoConfiguration() {
         load(EmptyConfiguration.class, TEST_DATA_ENV, "exomiser.hg19.data-version=1710", "exomiser.hg38.data-version=1710", "exomiser.phenotype.data-version=1710");
