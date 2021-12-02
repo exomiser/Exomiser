05aeaf79074ae42b4244d88a07e3a10c49441ff4
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-12-02 14:30:57 +0000

    Add new AcmgEvidence class

---
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/analysis/util/AcmgEvidence.java
@@ -0,0 +1,199 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2021 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.monarchinitiative.exomiser.core.analysis.util;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.monarchinitiative.exomiser.core.analysis.util.AcmgCriterion.Evidence;
+
+import javax.annotation.Nullable;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class AcmgEvidence {
+
+    @JsonProperty
+    private final Map<AcmgCriterion, Evidence> evidence;
+
+    private int pvs = 0;
+    private int ps = 0;
+    private int pm = 0;
+    private int pp = 0;
+
+    private int ba = 0;
+    private int bs = 0;
+    private int bp = 0;
+
+    private AcmgEvidence(Map<AcmgCriterion, Evidence> evidence) {
+        this.evidence = evidence == null || evidence.isEmpty() ? new EnumMap<>(AcmgCriterion.class) : new EnumMap<>(evidence);
+        countCriteriaEvidence(this.evidence);
+    }
+
+    @JsonCreator
+    public static AcmgEvidence of(Map<AcmgCriterion, Evidence> evidence) {
+        return new AcmgEvidence(evidence);
+    }
+
+    private void countCriteriaEvidence(Map<AcmgCriterion, Evidence> evidence) {
+        for (Map.Entry<AcmgCriterion, Evidence> entry : evidence.entrySet()) {
+            AcmgCriterion acmgCriterion = entry.getKey();
+            Evidence evidenceStrength = entry.getValue();
+            if (acmgCriterion.isPathogenic()) {
+                switch (evidenceStrength) {
+                    case VERY_STRONG:
+                        pvs++;
+                        break;
+                    case STRONG:
+                        ps++;
+                        break;
+                    case MODERATE:
+                        pm++;
+                        break;
+                    case SUPPORTING:
+                        pp++;
+                        break;
+                    default:
+                        // do nothing
+                        break;
+                }
+            }
+
+            if (acmgCriterion.isBenign()) {
+                switch (evidenceStrength) {
+                    case STAND_ALONE:
+                        ba++;
+                        break;
+                    case STRONG:
+                        bs++;
+                        break;
+                    case SUPPORTING:
+                        bp++;
+                        break;
+                    default:
+                        // do nothing
+                        break;
+                }
+            }
+        }
+    }
+
+    public boolean hasCriterion(AcmgCriterion acmgCriterion) {
+        return evidence.containsKey(acmgCriterion);
+    }
+
+    @Nullable
+    public Evidence criterionEvidence(AcmgCriterion acmgCriterion) {
+        return evidence.get(acmgCriterion);
+    }
+
+    public int size() {
+        return evidence.size();
+    }
+
+    public boolean isEmpty() {
+        return evidence.isEmpty();
+    }
+
+    public int pvs() {
+        return pvs;
+    }
+
+    public int ps() {
+        return ps;
+    }
+
+    public int pm() {
+        return pm;
+    }
+
+    public int pp() {
+        return pp;
+    }
+
+    public int ba() {
+        return ba;
+    }
+
+    public int bs() {
+        return bs;
+    }
+
+    public int bp() {
+        return bp;
+    }
+
+    public static AcmgEvidence.Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AcmgEvidence that = (AcmgEvidence) o;
+        return evidence.equals(that.evidence);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(evidence);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        for (Map.Entry<AcmgCriterion, Evidence> entry : evidence.entrySet()) {
+            AcmgCriterion acmgCriterion = entry.getKey();
+            Evidence assignedEvidence = entry.getValue();
+            if (acmgCriterion.evidence() != assignedEvidence) {
+                stringJoiner.add(acmgCriterion.toString() + '_' + assignedEvidence.displayString());
+            } else {
+                stringJoiner.add(acmgCriterion.toString());
+            }
+        }
+        return '[' + stringJoiner.toString() + ']';
+    }
+
+    public static class Builder {
+
+        private final EnumMap<AcmgCriterion, Evidence> evidence = new EnumMap<>(AcmgCriterion.class);
+
+        public Builder add(AcmgCriterion acmgCriterion) {
+            evidence.put(acmgCriterion, acmgCriterion.evidence());
+            return this;
+        }
+
+        public Builder add(AcmgCriterion acmgCriterion, Evidence evidence) {
+            this.evidence.put(acmgCriterion, evidence);
+            return this;
+        }
+
+        public boolean contains(AcmgCriterion acmgCriterion) {
+            return evidence.containsKey(acmgCriterion);
+        }
+
+        public AcmgEvidence build() {
+            return new AcmgEvidence(evidence);
+        }
+    }
+}
---
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/analysis/util/AcmgEvidenceTest.java
@@ -0,0 +1,194 @@
+/*
+ * The Exomiser - A tool to annotate and prioritize genomic variants
+ *
+ * Copyright (c) 2016-2021 Queen Mary University of London.
+ * Copyright (c) 2012-2016 Charité Universitätsmedizin Berlin and Genome Research Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.monarchinitiative.exomiser.core.analysis.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.monarchinitiative.exomiser.core.analysis.util.AcmgCriterion.*;
+
+public class AcmgEvidenceTest {
+
+    @Test
+    public void testToStringDefaultEvidence() {
+        AcmgEvidence instance = AcmgEvidence.builder().add(PVS1).add(PS1).build();
+        assertThat(instance.toString(), equalTo("[PVS1, PS1]"));
+    }
+
+    @Test
+    public void testToStringModifiedEvidence() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1, Evidence.STRONG)
+                .add(PS1, Evidence.MODERATE)
+                .add(PP3, Evidence.VERY_STRONG)
+                .build();
+        assertThat(instance.toString(), equalTo("[PVS1_Strong, PS1_Moderate, PP3_VeryStrong]"));
+    }
+
+    @Test
+    public void testGetEvidenceOverwriteInputInBuilder() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1, Evidence.STRONG)
+                .add(PVS1)
+                .build();
+        assertThat(instance.criterionEvidence(PVS1), equalTo(PVS1.evidence()));
+    }
+
+    @Test
+    public void testGetEvidenceDefaultValue() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1)
+                .build();
+        assertThat(instance.criterionEvidence(PVS1), equalTo(PVS1.evidence()));
+    }
+
+    @Test
+    public void testGetEvidenceModifiedValue() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1, Evidence.MODERATE)
+                .build();
+        assertThat(instance.criterionEvidence(PVS1), equalTo(Evidence.MODERATE));
+    }
+
+    @Test
+    public void testGetEvidenceNoValue() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1, Evidence.MODERATE)
+                .build();
+        assertThat(instance.criterionEvidence(PM3), equalTo(null));
+    }
+
+    @Test
+    public void testContainsEvidence() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1, Evidence.MODERATE)
+                .build();
+        assertThat(instance.hasCriterion(PVS1), equalTo(true));
+        assertThat(instance.hasCriterion(PM3), equalTo(false));
+    }
+
+    @Test
+    public void testBuilderContainsEvidence() {
+        AcmgEvidence.Builder instance = AcmgEvidence.builder()
+                .add(PVS1, Evidence.MODERATE);
+        assertThat(instance.contains(PVS1), equalTo(true));
+        assertThat(instance.contains(PM3), equalTo(false));
+    }
+
+    @Test
+    public void testSizeWhenEmpty() {
+        AcmgEvidence instance = AcmgEvidence.builder().build();
+        assertThat(instance.size(), equalTo(0));
+    }
+
+    @Test
+    public void testSizeWithElement() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1)
+                .build();
+        assertThat(instance.size(), equalTo(1));
+    }
+
+    @Test
+    public void testIsEmpty() {
+        AcmgEvidence instance = AcmgEvidence.builder().build();
+        assertThat(instance.isEmpty(), equalTo(true));
+    }
+
+    @Test
+    public void testNotEmpty() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1)
+                .build();
+        assertThat(instance.isEmpty(), equalTo(false));
+    }
+
+    @Test
+    void testPvs() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PVS1)
+                .add(PM1, Evidence.VERY_STRONG)
+                .build();
+        assertThat(instance.pvs(), equalTo(2));
+    }
+
+    @Test
+    void testPs() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PS1)
+                .build();
+        assertThat(instance.ps(), equalTo(1));
+    }
+
+    @Test
+    void testPm() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PM1)
+                .build();
+        assertThat(instance.pm(), equalTo(1));
+    }
+
+    @Test
+    void testPp() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(PP1)
+                .build();
+        assertThat(instance.pp(), equalTo(1));
+    }
+
+    @Test
+    void testBa() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(BA1)
+                .build();
+        assertThat(instance.ba(), equalTo(1));
+    }
+
+    @Test
+    void testBs() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(BS1)
+                .build();
+        assertThat(instance.bs(), equalTo(1));
+    }
+
+    @Test
+    void testBp() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .add(BP1)
+                .build();
+        assertThat(instance.bp(), equalTo(1));
+    }
+
+    @Test
+    void testEmptyCounts() {
+        AcmgEvidence instance = AcmgEvidence.builder()
+                .build();
+        assertThat(instance.pvs(), equalTo(0));
+        assertThat(instance.ps(), equalTo(0));
+        assertThat(instance.pm(), equalTo(0));
+        assertThat(instance.pp(), equalTo(0));
+        assertThat(instance.ba(), equalTo(0));
+        assertThat(instance.bs(), equalTo(0));
+        assertThat(instance.bp(), equalTo(0));
+    }
+}
