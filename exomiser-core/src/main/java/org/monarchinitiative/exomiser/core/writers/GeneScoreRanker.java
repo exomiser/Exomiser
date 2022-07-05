c03c0fd4a9916f5c47f89a71d40434e6bc7a1dea
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2022-07-05 18:30:18 +0100

    Add new GeneScoreRanker to help with producing ranked gene scores for Writers

---
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/writers/GeneScoreRanker.java
@@ -0,0 +1,187 @@
+package org.monarchinitiative.exomiser.core.writers;
+
+import de.charite.compbio.jannovar.mendel.ModeOfInheritance;
+import org.monarchinitiative.exomiser.core.analysis.AnalysisResults;
+import org.monarchinitiative.exomiser.core.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toUnmodifiableMap;
+
+class GeneScoreRanker {
+
+    private static final Logger logger = LoggerFactory.getLogger(GeneScoreRanker.class);
+
+    private final OutputSettings outputSettings;
+    private final boolean contributingVariantsOnly;
+
+    private final List<Gene> filteredGenesForOutput;
+    private final Map<GeneIdentifier, Gene> genesById;
+
+    GeneScoreRanker(AnalysisResults analysisResults, OutputSettings outputSettings) {
+        this.outputSettings = outputSettings;
+        this.contributingVariantsOnly = outputSettings.outputContributingVariantsOnly();
+        this.filteredGenesForOutput = outputSettings.filterGenesForOutput(analysisResults.getGenes());
+        this.genesById = filteredGenesForOutput.stream().collect(toUnmodifiableMap(Gene::getGeneIdentifier, Function.identity()));
+    }
+
+    Map<GeneIdentifier, Gene> mapGenesByGeneIdentifier() {
+        return genesById;
+    }
+
+    private List<GeneScore> calculateRankedGeneScores() {
+        Map<Boolean, List<GeneScore>> rankedAndUnrankedGeneScores = filteredGenesForOutput.stream()
+                .flatMap(gene -> {
+                    List<GeneScore> compatibleGeneScores = new ArrayList<>(gene.getCompatibleGeneScores());
+                    if (gene.getVariantEvaluations().stream().anyMatch(ve -> ve.getFilterStatus() == FilterStatus.FAILED)) {
+                        // create a failed gene score placeholder for when run in FULL mode
+                        GeneScore geneScore = GeneScore.builder()
+                                .geneIdentifier(gene.getGeneIdentifier())
+                                .modeOfInheritance(ModeOfInheritance.ANY)
+                                .combinedScore(0)
+                                .phenotypeScore(gene.getPriorityScore())
+                                .variantScore(0)
+                                .build();
+                        compatibleGeneScores.add(geneScore);
+                    }
+                    return compatibleGeneScores.stream();
+                })
+                .sorted()
+                .collect(partitioningBy(o -> o.getCombinedScore() != 0));
+
+        if (outputSettings.outputContributingVariantsOnly()) {
+            return rankedAndUnrankedGeneScores.get(true);
+        } else {
+            List<GeneScore> rankedGeneScores = new ArrayList<>(rankedAndUnrankedGeneScores.get(true).size() + rankedAndUnrankedGeneScores.get(false).size());
+            rankedGeneScores.addAll(rankedAndUnrankedGeneScores.get(true));
+            rankedGeneScores.addAll(rankedAndUnrankedGeneScores.get(false));
+            return List.copyOf(rankedGeneScores);
+        }
+    }
+
+    Stream<RankedGene> rankedGenes() {
+        ScoreRanker scoreRanker = new ScoreRanker(4);
+        return calculateRankedGeneScores().stream()
+                .map(geneScore -> {
+                    int rank = scoreRanker.rank(geneScore.getCombinedScore());
+                    return new RankedGene(rank, genesById.get(geneScore.getGeneIdentifier()), geneScore);
+                });
+    }
+
+    Stream<RankedVariant> rankedVariants() {
+        return rankedGenes().flatMap(rankedGene -> {
+            int rank = rankedGene.rank();
+            GeneScore geneScore = rankedGene.geneScore();
+            ModeOfInheritance modeOfInheritance = geneScore.getModeOfInheritance();
+            logger.debug("{} {} {} {} {} {}", rank, geneScore.getGeneIdentifier().getGeneSymbol(), modeOfInheritance.getAbbreviation(), geneScore.getCombinedScore(), geneScore.getPhenotypeScore(), geneScore.getVariantScore());
+            // a GeneScore only contains the contributing variants so can't be used directly to get the variants involved, hence the requirement for the Gene.
+            return rankedGene.gene()
+                    .getVariantEvaluations().stream()
+                    .filter(variantEvaluation -> !contributingVariantsOnly || variantEvaluation.contributesToGeneScoreUnderMode(modeOfInheritance))
+                    .filter(variantEvaluation -> variantEvaluation.isCompatibleWith(modeOfInheritance))
+                    .filter(variantEvaluation -> (geneScore.getCombinedScore() == 0) != variantEvaluation.passedFilters())
+                    .sorted(VariantEvaluation::compareByRank)
+                    .map(ve -> new RankedVariant(rank, ve, geneScore));
+        });
+    }
+
+    static class RankedGene {
+        private final int rank;
+        private final Gene gene;
+        private final GeneScore geneScore;
+
+        public RankedGene(int rank, Gene gene, GeneScore geneScore) {
+            this.rank = rank;
+            this.gene = Objects.requireNonNull(gene);
+            this.geneScore = Objects.requireNonNull(geneScore);
+        }
+
+        public int rank() {
+            return rank;
+        }
+
+        public Gene gene() {
+            return gene;
+        }
+
+        public GeneScore geneScore() {
+            return geneScore;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RankedGene that = (RankedGene) o;
+            return rank == that.rank && gene.equals(that.gene) && geneScore.equals(that.geneScore);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(rank, gene, geneScore);
+        }
+
+        @Override
+        public String toString() {
+            return "RankedGene{" +
+                    "rank=" + rank +
+                    ", gene=" + gene +
+                    ", geneScore=" + geneScore +
+                    '}';
+        }
+    }
+
+    static class RankedVariant {
+        private final int rank;
+        private final VariantEvaluation variantEvaluation;
+        private final GeneScore geneScore;
+
+        public RankedVariant(int rank, VariantEvaluation variantEvaluation, GeneScore geneScore) {
+            this.rank = rank;
+            this.variantEvaluation = Objects.requireNonNull(variantEvaluation);
+            this.geneScore = Objects.requireNonNull(geneScore);
+        }
+
+        public int rank() {
+            return rank;
+        }
+
+        public VariantEvaluation variantEvaluation() {
+            return variantEvaluation;
+        }
+
+        public GeneScore geneScore() {
+            return geneScore;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RankedVariant that = (RankedVariant) o;
+            return rank == that.rank && variantEvaluation.equals(that.variantEvaluation) && geneScore.equals(that.geneScore);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(rank, variantEvaluation, geneScore);
+        }
+
+        @Override
+        public String toString() {
+            return "RankedVariant{" +
+                    "rank=" + rank +
+                    ", variantEvaluation=" + variantEvaluation +
+                    ", geneScore=" + geneScore +
+                    '}';
+        }
+    }
+}
---
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/writers/GeneScoreRankerTest.java
@@ -0,0 +1,242 @@
+package org.monarchinitiative.exomiser.core.writers;
+
+import de.charite.compbio.jannovar.mendel.ModeOfInheritance;
+import org.junit.jupiter.api.Test;
+import org.monarchinitiative.exomiser.core.analysis.AnalysisResults;
+import org.monarchinitiative.exomiser.core.filters.FilterResult;
+import org.monarchinitiative.exomiser.core.filters.FilterType;
+import org.monarchinitiative.exomiser.core.genome.GenomeAssembly;
+import org.monarchinitiative.exomiser.core.genome.TestFactory;
+import org.monarchinitiative.exomiser.core.genome.TestVariantFactory;
+import org.monarchinitiative.exomiser.core.model.Gene;
+import org.monarchinitiative.exomiser.core.model.GeneIdentifier;
+import org.monarchinitiative.exomiser.core.model.GeneScore;
+import org.monarchinitiative.exomiser.core.model.VariantEvaluation;
+import org.monarchinitiative.exomiser.core.prioritisers.HiPhivePriorityResult;
+import org.monarchinitiative.exomiser.core.prioritisers.MockPriorityResult;
+import org.monarchinitiative.exomiser.core.prioritisers.PriorityType;
+import org.monarchinitiative.svart.CoordinateSystem;
+import org.monarchinitiative.svart.Position;
+import org.monarchinitiative.svart.Strand;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.in;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GeneScoreRankerTest {
+
+    @Test
+    public void mapGenesByGeneIdentifier() {
+        var fgfr2 = TestFactory.newGeneFGFR2();
+        var gnrhr2 = TestFactory.newGeneGNRHR2();
+        AnalysisResults analysisResults = AnalysisResults.builder().genes(List.of(fgfr2, gnrhr2)).build();
+
+        GeneScoreRanker instance = new GeneScoreRanker(analysisResults, OutputSettings.defaults());
+
+        Map<GeneIdentifier, Gene> expected = Map.of(fgfr2.getGeneIdentifier(), fgfr2, gnrhr2.getGeneIdentifier(), gnrhr2);
+        assertThat(instance.mapGenesByGeneIdentifier(), equalTo(expected));
+    }
+
+    @Test
+    public void rankedGenes() {
+        var fgfr2 = TestFactory.newGeneFGFR2();
+        var topGeneScore = GeneScore.builder()
+                .combinedScore(1.0)
+                .geneIdentifier(fgfr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        fgfr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        fgfr2.addGeneScore(topGeneScore);
+
+        var gnrhr2 = TestFactory.newGeneGNRHR2();
+        var secondGeneScore = GeneScore.builder()
+                .combinedScore(0.9)
+                .geneIdentifier(gnrhr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        gnrhr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        gnrhr2.addGeneScore(secondGeneScore);
+
+        AnalysisResults analysisResults = AnalysisResults.builder().genes(List.of(fgfr2, gnrhr2)).build();
+
+        GeneScoreRanker instance = new GeneScoreRanker(analysisResults, OutputSettings.defaults());
+        List<GeneScoreRanker.RankedGene> expected = List.of(
+                new GeneScoreRanker.RankedGene(1, fgfr2, topGeneScore),
+                new GeneScoreRanker.RankedGene(2, gnrhr2, secondGeneScore)
+        );
+        assertThat(instance.rankedGenes().collect(toList()), equalTo(expected));
+    }
+
+    @Test
+    public void rankedGenesWithFailedVariant() {
+        var fgfr2 = TestFactory.newGeneFGFR2();
+        var topGeneScore = GeneScore.builder()
+                .combinedScore(1.0)
+                .geneIdentifier(fgfr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        fgfr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        fgfr2.addGeneScore(topGeneScore);
+
+        VariantEvaluation failedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(10), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(1234567), "G", "T")
+                .filterResults(FilterResult.fail(FilterType.FREQUENCY_FILTER))
+                .build();
+        fgfr2.addVariant(failedVariant);
+
+        var failedGeneScore = GeneScore.builder()
+                .combinedScore(0.0)
+                .geneIdentifier(fgfr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.ANY)
+                .build();
+
+
+        var gnrhr2 = TestFactory.newGeneGNRHR2();
+        var secondGeneScore = GeneScore.builder()
+                .combinedScore(0.9)
+                .geneIdentifier(gnrhr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        gnrhr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        gnrhr2.addGeneScore(secondGeneScore);
+
+        AnalysisResults analysisResults = AnalysisResults.builder().genes(List.of(fgfr2, gnrhr2)).build();
+
+        GeneScoreRanker instance = new GeneScoreRanker(analysisResults, OutputSettings.defaults());
+        List<GeneScoreRanker.RankedGene> expected = List.of(
+                new GeneScoreRanker.RankedGene(1, fgfr2, topGeneScore),
+                new GeneScoreRanker.RankedGene(2, gnrhr2, secondGeneScore),
+                new GeneScoreRanker.RankedGene(3, fgfr2, failedGeneScore)
+        );
+        assertThat(instance.rankedGenes().collect(toList()), equalTo(expected));
+    }
+
+    @Test
+    public void rankedVariants() {
+        var fgfr2 = TestFactory.newGeneFGFR2();
+        var topGeneScore = GeneScore.builder()
+                .combinedScore(1.0)
+                .geneIdentifier(fgfr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        fgfr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        fgfr2.addPriorityResult(new MockPriorityResult(PriorityType.HIPHIVE_PRIORITY, fgfr2.getEntrezGeneID(), fgfr2.getGeneSymbol(), 1.0));
+        fgfr2.addGeneScore(topGeneScore);
+
+        VariantEvaluation fgfr2TopRankedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(10), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(1234567), "G", "T")
+                .filterResults(FilterResult.pass(FilterType.FREQUENCY_FILTER))
+                .compatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .contributingModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .build();
+        fgfr2.addVariant(fgfr2TopRankedVariant);
+
+
+        VariantEvaluation fgfr2FailedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(10), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(1234567), "G", "T")
+                .filterResults(FilterResult.fail(FilterType.FREQUENCY_FILTER))
+                .compatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .build();
+        fgfr2.addVariant(fgfr2FailedVariant);
+
+        var failedGeneScore = GeneScore.builder()
+                .combinedScore(0.0)
+                .geneIdentifier(fgfr2.getGeneIdentifier())
+                .phenotypeScore(fgfr2.getPriorityScore())
+                .modeOfInheritance(ModeOfInheritance.ANY)
+                .build();
+
+
+        var gnrhr2 = TestFactory.newGeneGNRHR2();
+        var secondGeneScore = GeneScore.builder()
+                .combinedScore(0.9)
+                .geneIdentifier(gnrhr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        gnrhr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        gnrhr2.addGeneScore(secondGeneScore);
+
+        VariantEvaluation gnrhr2TopRankedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(7), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(654321), "G", "T")
+                .filterResults(FilterResult.pass(FilterType.FREQUENCY_FILTER))
+                .compatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .contributingModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .build();
+        gnrhr2.addVariant(gnrhr2TopRankedVariant);
+
+
+        AnalysisResults analysisResults = AnalysisResults.builder().genes(List.of(fgfr2, gnrhr2)).build();
+
+        GeneScoreRanker instance = new GeneScoreRanker(analysisResults, OutputSettings.defaults());
+        var expected = List.of(
+                new GeneScoreRanker.RankedVariant(1, fgfr2TopRankedVariant, topGeneScore),
+                new GeneScoreRanker.RankedVariant(2, gnrhr2TopRankedVariant, secondGeneScore),
+                new GeneScoreRanker.RankedVariant(3, fgfr2FailedVariant, failedGeneScore)
+        );
+        assertThat(instance.rankedVariants().collect(toList()), equalTo(expected));
+    }
+
+    @Test
+    public void rankedContributingVariants() {
+        var fgfr2 = TestFactory.newGeneFGFR2();
+        var topGeneScore = GeneScore.builder()
+                .combinedScore(1.0)
+                .geneIdentifier(fgfr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        fgfr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        fgfr2.addPriorityResult(new MockPriorityResult(PriorityType.HIPHIVE_PRIORITY, fgfr2.getEntrezGeneID(), fgfr2.getGeneSymbol(), 1.0));
+        fgfr2.addGeneScore(topGeneScore);
+
+        VariantEvaluation fgfr2TopRankedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(10), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(1234567), "G", "T")
+                .filterResults(FilterResult.pass(FilterType.FREQUENCY_FILTER))
+                .compatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .contributingModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .build();
+        fgfr2.addVariant(fgfr2TopRankedVariant);
+
+
+        VariantEvaluation fgfr2FailedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(10), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(1234567), "G", "T")
+                .filterResults(FilterResult.fail(FilterType.FREQUENCY_FILTER))
+                .compatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .build();
+        fgfr2.addVariant(fgfr2FailedVariant);
+
+
+        var gnrhr2 = TestFactory.newGeneGNRHR2();
+        var secondGeneScore = GeneScore.builder()
+                .combinedScore(0.9)
+                .geneIdentifier(gnrhr2.getGeneIdentifier())
+                .modeOfInheritance(ModeOfInheritance.AUTOSOMAL_DOMINANT)
+                .build();
+        gnrhr2.setCompatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT));
+        gnrhr2.addGeneScore(secondGeneScore);
+
+        VariantEvaluation gnrhr2TopRankedVariant = VariantEvaluation.builder()
+                .with(GenomeAssembly.HG19.getContigById(7), "", Strand.POSITIVE, CoordinateSystem.oneBased(), Position.of(654321), "G", "T")
+                .filterResults(FilterResult.pass(FilterType.FREQUENCY_FILTER))
+                .compatibleInheritanceModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .contributingModes(Set.of(ModeOfInheritance.AUTOSOMAL_DOMINANT))
+                .build();
+        gnrhr2.addVariant(gnrhr2TopRankedVariant);
+
+
+        AnalysisResults analysisResults = AnalysisResults.builder().genes(List.of(fgfr2, gnrhr2)).build();
+
+        GeneScoreRanker instance = new GeneScoreRanker(analysisResults, OutputSettings.builder().outputContributingVariantsOnly(true).build());
+        var expected = List.of(
+                new GeneScoreRanker.RankedVariant(1, fgfr2TopRankedVariant, topGeneScore),
+                new GeneScoreRanker.RankedVariant(2, gnrhr2TopRankedVariant, secondGeneScore)
+        );
+        assertThat(instance.rankedVariants().collect(toList()), equalTo(expected));
+    }
+}
