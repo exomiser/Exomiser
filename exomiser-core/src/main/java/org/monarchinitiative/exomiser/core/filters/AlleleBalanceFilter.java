5fdb1969f2034621cb99ff9a6ea29b003068ec36
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2025-12-09 10:23:15 +0000

    Update AlleleBalanceFilter to only apply check for het genotypes.

--- exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/filters/AlleleBalanceFilter.java
+++ exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/filters/AlleleBalanceFilter.java
@@ -72,9 +72,10 @@ public record AlleleBalanceFilter() implements VariantFilter {
         // AB (allele balance) = alternate reads / (alternate reads + reference reads)
         // AB = AD / DP (where AD needs to match the allele according to GT)
         // ##INFO=<ID=AF,Number=A,Type=Float,Description="Allele Frequency, for each ALT allele, in the same order as listed">
-        double ab = calculateAlleleBalance(allAlleles, genotype);
+        double ab = calculateGenotypeAlleleBalance(genotype, allAlleles);
         if (ab == -1.0) {
             // no genotype / read quality info available, so pass the variant.
+            logger.debug("FAIL AB {} for {}", ab, genotype);
             return true;
         }
         boolean passesABCheck = true;
@@ -86,36 +87,89 @@ public record AlleleBalanceFilter() implements VariantFilter {
                 // fail if AF < 0.2 or AF > 0.8 (Quinlan recommended, but for GEL solved cases the UDN recommended 0.15-0.85 removed fewer diagnosed cases)
                 passesABCheck = ab >= 0.15 && ab <= 0.85;
             }
-        } else if (genotype.isHomRef()) {
-            // fail if AF > 0.02 (Quinlan recommended, but for GEL solved cases where the NR was close to 30 a single read would remove the diagnosis)
-            passesABCheck = ab <= 0.03;
         }
+        // ordinarily fail a homRef (0/0) if AF > 0.02 (Quinlan recommended, but for GEL solved cases where the NR was close to 30 a single read would remove the diagnosis)
         logger.debug("{} AB {} for {}", (passesABCheck ? "PASS" : "FAIL"), ab, genotype);
         return passesABCheck;
     }
 
-    private double calculateAlleleBalance(List<Allele> allAlleles, Genotype genotype) {
+    private double calculateGenotypeAlleleBalance(Genotype genotype, List<Allele> allAlleles) {
+        // this is retarded, but the HTSJDK doesn't store the allele index of each allele, so the GT=0/1 1/1 0/2
+        // indexes are lost and need to be recalculated so that the AD for a specific allele can be found for
+        // multi-alleleic sites.
+        // create an ordered array of the alleles present in the genotype, as these are not stored
+        // e.g. 0/0 = [0,0], 1/0 or 0/1 = [0,1], 1/1 = [1,1], 1/2 = [1,2]...
+        int[] genotypeAlleleIndex = buildGenotypeAlleleIndex(genotype, allAlleles);
+
         if (genotype.hasAD()) {
-            return calcABfromAD(allAlleles, genotype);
+            return calcABfromAD(genotype, genotypeAlleleIndex);
         }
         // Try using NV & NR (Platypus) AB = NV / NR
         // https://github.com/andyrimmer/Platypus/blob/master/misc/README.txt
         // NR Number of reads covering variant position in this sample
         // NV Number of reads at variant position which support the called variant in this sample
+
+        // See also: https://github.com/andyrimmer/Platypus/issues/61#issuecomment-279744487 for multi-allelic sites
+        // "The NR and NV values are given for each allele, in the order which the alleles are listed in the ALT column.
+        // So the NR values will always be the same (number of reads supporting the reference), and the NV values will
+        // be different for each variant allele. These numbers are harder to interpret when you have e.g. repetitive
+        // indels because some reads could support either variant allele."
         Map<String, Object> extendedAttributes = genotype.getExtendedAttributes();
         if (extendedAttributes.containsKey("NR") && extendedAttributes.containsKey("NV")) {
-            int nr = Integer.parseInt((String) extendedAttributes.get("NR"));
-            int nv = Integer.parseInt((String) extendedAttributes.get("NV"));
-            return nv / Math.max(nr, 1.0);
+            String nrString = (String) extendedAttributes.get("NR");
+            String nvString = (String) extendedAttributes.get("NV");
+
+            var nvStrings = nvString.contains(",") ? nvString.split(",") : new String[]{nvString};
+            if (allAlleles.size() - 1 != (nvStrings.length)) {
+                logger.debug("FAIL allele count {} != NV count {}", allAlleles.size(), nvStrings.length);
+                // This case should FAIL as the number of ALT alleles doesn't match the genotype info, but we're using
+                //  this as a soft filter where we'll ignore incorrect or absent data to maximise recall at the
+                //  expense of some precision
+                return -1.0;
+            }
+            // make a new AD-like array
+            int[] alleleDepths = new int[nvStrings.length + 1];
+            int nr = Integer.parseInt(nrString.contains(",") ? nrString.substring(0, nrString.indexOf(",")) : nrString);
+            alleleDepths[0] = nr;
+            for (int i = 0; i < nvStrings.length; i++) {
+                alleleDepths[i + 1] = Integer.parseInt(nvStrings[i]);
+            }
+
+//            int nv = alleleDepths[genotypeAlleleIndex[genotypeAlleleIndex.length - 1]];
+            int nv = 0;
+            for (int i = 1; i < alleleDepths.length; i++) {
+                nv += alleleDepths[i];
+            }
+            double ab = nv / Math.max(nr, 1.0);
+            logger.debug("NR={}, NV={}, AB={} for {}", nr, nv, ab, genotype);
+            return ab;
         }
         return -1.0;
     }
 
-    private static double calcABfromAD(List<Allele> allAlleles, Genotype genotype) {
+    private static double calcABfromAD(Genotype genotype, int[] genotypeAlleleIndexes) {
         int[] ad = genotype.getAD();
-        // this is retarded, but the HTSJDK doesn't store the allele index of each allele, so the GT=0/1 1/1 0/2
-        // indexes are lost and need to be recalculated so that the AD for a specific allele can be found for
-        // multi-alleleic sites
+        int numRefReads = ad[0];
+        // should this not be the sum of all alternate alleles?
+        int numAltReads = 0;
+        for (int i = 1; i < ad.length; i++) {
+            numAltReads += ad[i];
+        }
+        double ab = numAltReads / Math.max(numAltReads + (double) numRefReads, 1.0);
+        logger.debug("AD REF={}, AD ALT={}, AB={} for {}", numRefReads, numAltReads, ab, genotype);
+        return ab;
+    }
+
+    /**
+     * Builds an array representing indexes of alleles from the genotype in the list of all alleles.
+     * The resulting array is sorted to ensure a consistent order. For example, 0/1 or 1/0 or 1|0 will return the array
+     * [0, 1], a 1/2 or 2/1 genotype will return [1, 2].
+     *
+     * @param genotype the genotype containing the alleles to be indexed
+     * @param allAlleles the complete list of alleles from which the indexes are derived
+     * @return an array of indexes for each allele in the genotype, sorted in ascending order
+     */
+    private static int[] buildGenotypeAlleleIndex(Genotype genotype, List<Allele> allAlleles) {
         List<Allele> genotypeAlleles = genotype.getAlleles();
         // e.g. [1], [0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2]
         int[] alleleIndexes = new int[genotypeAlleles.size()];
@@ -124,9 +178,7 @@ public record AlleleBalanceFilter() implements VariantFilter {
         }
         // ensure 1/0 is ordered as 0, 1
         Arrays.sort(alleleIndexes);
-        int numRefReads = ad[0];
-        int numAltReads = ad[alleleIndexes[alleleIndexes.length - 1]];
-        return numAltReads / Math.max(numAltReads + (double) numRefReads, 1.0);
+        return alleleIndexes;
     }
 
 }
--- exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/filters/AlleleBalanceFilterTest.java
+++ exomiser-core/src/test/java/org/monarchinitiative/exomiser/core/filters/AlleleBalanceFilterTest.java
@@ -35,6 +35,7 @@ class AlleleBalanceFilterTest {
 
     @ParameterizedTest
     @CsvSource({
+            "PASS, '1       47607851        rs2056899       A       T       1826.78 .      .;    NA     .   .       .       .",
             "PASS, '1       47607851        rs2056899       A       T       1826.78 .      .;    GT   1/1    0/1    0/1    1/1",
             "PASS, '1       47607851        rs2056899       A       T       1826.78 .      .;    GT:AD:DP:GQ:PL   1/1:0,45:45:99:1253,132,0    0/1:6,8:14:99:197,0,182    0/1:12,4:16:66:66,0,346    1/1:0,13:13:36:357,36,0'",
             "PASS, '1       47607851        rs2056899       A       T       1826.78 .      .;    GT:NR:NV:GQ:PL   1/1:45:45:99:1253,132,0    0/1:14:8:99:197,0,182    0/1:16:4:66:66,0,346    1/1:13:13:36:357,36,0'",
@@ -44,8 +45,12 @@ class AlleleBalanceFilterTest {
             "FAIL, '1       47123898        rs67089539      TAAA    TAAAA,T 382.01  .    AC=1,2;AF=0.125,0.25;AN=8;BaseQRankSum=-2.349;DB;DP=66       GT:AD:DP:GQ:PL  0/0:1,0,0:4:5:0,5,20,6,23,31    0/0:18,0,0:21:13:0,13,306,51,356,783    0/2:14,0,3:20:99:116,140,259,0,102,280  1/2:8,0,4:21:99:362,206,248,163,0,482'",
             "FAIL, '1       70904523        rs112004441     T       TTG,TTGTG       1285.02 .    AC=3,4;AF=0.375,0.5;AN=8     GT:AD:DP:GQ:PL  1/2:10,5,6:23:99:555,231,199,192,0,162    1/2:12,1,4:17:30:272,146,131,44,0,30    1/2:6,1,4:11:28:288,148,133,42,0,28    0/2:15,0,2:20:99:297,180,279,0,120,156'",
             "FAIL, 'MT      13227           .               C        CT        .        PASS     DP=20656;MQ=60 GT:AD:AF:DP:FT:LOD:F1R2:F2R1 0/1:7397,13:0.002:7410:PASS:17.11:3497,5:3900,8 0/0:7309,9:0.001:6772:PASS:.:.:. 0/0:7066,5:0.001:6351:PASS:.:.:. 0/0:7066,5:0.001:6351:PASS:.:.:.'",
+            // Some GEL samples were decomposed from multi-allelic sites into mono-allelic sites, but the read information wasn't split, hence the NR=45,45:NV=45,45 where the numerical value of NV or NR is listed for each alternate allele. In this case the data is incorrect, but it will 'PASS' as were using it as a soft filter.
+            "PASS, '1       47607851        rs2056899       A       T       1826.78 .      .;    GT:NR:NV:GQ:PL   1/1:45,45:45,45:99:1253,132,0    0/1:14,14:8,8:99:197,0,182    0/1:16,16:4,4:66:66,0,346    1/1:13,13:13,13:36:357,36,0'",
+            // 0/0 genotype AB should be ignored as even cases where a single ALT read from a 30x WGS will fail, as in this case:
+            "PASS, '1       47607851        rs2056899       A       T       1826.78 .      .;    GT:GQ:NR:NV   0/0:99:32:1   1/1:28:35:14    0/1:99:34:9    0/1:99:34:9'",
     })
-    void runFilter(FilterResult.Status filterStatus, String vcfRecord) {
+    void runFilter(FilterResult.Status expectedStatus, String vcfRecord) {
         TestVcfReader testVcfReader = TestVcfReader.of(List.of("sample1", "sample2", "sample3", "sample4"));
         VariantContext variantContext = testVcfReader.readVariantContext(vcfRecord);
         VariantContextConverter variantContextConverter = VariantContextConverter.of(GenomicAssemblies.GRCh37p13(), VariantTrimmer.leftShiftingTrimmer(VariantTrimmer.retainingCommonBase()));
@@ -58,10 +63,9 @@ class AlleleBalanceFilterTest {
                     .variantContext(variantContext)
                     .altAlleleId(i)
                     .build();
-            System.out.println(variantEvaluation);
             AlleleBalanceFilter instance = new AlleleBalanceFilter();
             FilterResult.Status status = instance.runFilter(variantEvaluation).status();
-            assertThat(status, equalTo(filterStatus));
+            assertThat(status, equalTo(expectedStatus));
         }
     }
 }
