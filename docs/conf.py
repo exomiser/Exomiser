641a737f4c072e5f0d7d4829245d369aaf1dca02
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2026-07-09 19:11:42 +0100

    Update version to 15.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ copyright = u'2026, Jules Jacobsen, Damian Smedley, Peter Robinson'
 author = u'Jules Jacobsen, Damian Smedley, Peter Robinson'
 
 # The short X.Y version
-version = u'15.1.0'
+version = u'15.1.1'
 genome_data_version = u'2602'
 phenotype_data_version = u'2602'
 # The full version, including alpha/beta/rc tags
